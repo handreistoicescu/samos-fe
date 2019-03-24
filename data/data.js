b58162f268df6ea578e55f4563eb8cc2e28104f3
Drei <stoicescuofsuburbia@gmail.com>
2019-03-24 20:27:37 +0200

    Updated events

--- data/data.js
+++ data/data.js
@@ -1,9 +1,205 @@
 exports.events = [
   {
+    "act": "OMG! Organisme Modificate Genetic",
+    "date": "2019-03-25",
+    "type": "Theatre",
+    "eventUrl": "https://www.facebook.com/events/674627602995115",
+    "venueName": "Casa Tranzit"
+  },
+  {
     "act": "Románia 100",
     "date": "2019-03-25",
     "type": "Theatre",
     "eventUrl": "https://www.facebook.com/events/820285381640385",
     "venueName": "ZUG.zone"
+  },
+  {
+    "act": "Chiritza în concert",
+    "date": "2019-03-26",
+    "type": "Theatre",
+    "eventUrl": "https://www.facebook.com/events/768223580243211",
+    "venueName": "Teatrul Naţional Cluj-Napoca"
+  },
+  {
+    "act": "If a Tree Were to Fall",
+    "date": "2019-03-26",
+    "type": "Art Performance",
+    "eventUrl": "https://www.facebook.com/events/632368963853934",
+    "venueName": "Fabrica de Pensule"
+  },
+  {
+    "act": "Rândul 3, aproape de margine",
+    "date": "2019-03-27",
+    "type": "Theatre",
+    "eventUrl": "https://www.facebook.com/events/235288567305262",
+    "venueName": "REACTOR de creație și experiment"
+  },
+  {
+    "act": "Sense",
+    "date": "2019-03-27",
+    "type": "Concert",
+    "eventUrl": "https://www.facebook.com/events/2291649627782100",
+    "venueName": "Sisters"
+  },
+  {
+    "act": "Odious painting for odious times",
+    "date": "2019-03-28",
+    "type": "Vernissage",
+    "eventUrl": "https://www.facebook.com/events/161588834749211",
+    "venueName": "White Cuib"
+  },
+  {
+    "act": "Ted Amber",
+    "date": "2019-03-28",
+    "type": "DJ Set",
+    "eventUrl": "https://www.facebook.com/events/213982196151697",
+    "venueName": "Gazette"
+  },
+  {
+    "act": "Povești din mileniul trecut",
+    "date": "2019-03-29",
+    "type": "Art Installation",
+    "eventUrl": "https://www.facebook.com/events/2120630374844274",
+    "venueName": "REACTOR de creație și experiment"
+  },
+  {
+    "act": "From the belly of a circle comes out a square",
+    "date": "2019-03-29",
+    "type": "Vernissage",
+    "eventUrl": "https://www.facebook.com/matcaartspace",
+    "venueName": "Matca Artspace"
+  },
+  {
+    "act": "Stefan Geiger",
+    "date": "2019-03-29",
+    "type": "Concert",
+    "eventUrl": "https://www.facebook.com/events/404580056967222",
+    "venueName": "Filarmonica de Stat Transilvania"
+  },
+  {
+    "act": "Taraf de Caliu",
+    "date": "2019-03-29",
+    "type": "Concert",
+    "eventUrl": "https://www.facebook.com/events/753661408343757",
+    "venueName": "Form Space"
+  },
+  {
+    "act": "Alci",
+    "date": "2019-03-29",
+    "type": "DJ Set",
+    "eventUrl": "https://www.facebook.com/events/2226825930712974",
+    "venueName": "Sequence"
+  },
+  {
+    "act": "Dragos Ilici",
+    "date": "2019-03-29",
+    "type": "DJ Set",
+    "eventUrl": "https://www.facebook.com/events/1998941073537830",
+    "venueName": "Gazette"
+  },
+  {
+    "act": "Dragutesku",
+    "date": "2019-03-29",
+    "type": "DJ Set",
+    "eventUrl": "https://www.facebook.com/events/432395370835935",
+    "venueName": "SubForm"
+  },
+  {
+    "act": "Simina Grigoriu",
+    "date": "2019-03-29",
+    "type": "DJ Set",
+    "eventUrl": "https://www.facebook.com/events/413324292771511",
+    "venueName": "Cluj Arena"
+  },
+  {
+    "act": "99,6%",
+    "date": "2019-03-30",
+    "type": "Theatre",
+    "eventUrl": "https://www.facebook.com/events/2200514626876985",
+    "venueName": "REACTOR de creație și experiment"
+  },
+  {
+    "act": "Clasa noastră",
+    "date": "2019-03-30",
+    "type": "Theatre",
+    "eventUrl": "https://www.facebook.com/events/1931325953663819",
+    "venueName": "Teatrul Naţional Cluj-Napoca"
+  },
+  {
+    "act": "Mihai Viteazul // Tranziții",
+    "date": "2019-03-30",
+    "type": "Theatre",
+    "eventUrl": "https://www.facebook.com/events/364525007728012",
+    "venueName": "Casa Tranzit"
+  },
+  {
+    "act": "Skyharbor",
+    "date": "2019-03-30",
+    "type": "Concert",
+    "eventUrl": "https://www.facebook.com/events/1051550408365029",
+    "venueName": "Flying Circus"
+  },
+  {
+    "act": "Bondax",
+    "date": "2019-03-30",
+    "type": "DJ Set",
+    "eventUrl": "https://www.facebook.com/events/550531965468174",
+    "venueName": "Sala Polivalentă"
+  },
+  {
+    "act": "Nick Curly",
+    "date": "2019-03-30",
+    "type": "DJ Set",
+    "eventUrl": "https://www.facebook.com/events/111699946514565",
+    "venueName": "Form Space"
+  },
+  {
+    "act": "99,6%",
+    "date": "2019-03-31",
+    "type": "Theatre",
+    "eventUrl": "https://www.facebook.com/events/2200514626876985",
+    "venueName": "REACTOR de creație și experiment"
+  },
+  {
+    "act": "ZÉRÓ",
+    "date": "2019-03-31",
+    "type": "Theatre",
+    "eventUrl": "https://www.facebook.com/events/2392535474309119",
+    "venueName": "ZUG.zone"
+  },
+  {
+    "act": "ZÉRÓ",
+    "date": "2019-04-01",
+    "type": "Theatre",
+    "eventUrl": "https://www.facebook.com/events/508225239583941",
+    "venueName": "ZUG.zone"
+  },
+  {
+    "act": "Window",
+    "date": "2019-04-04",
+    "type": "Concert",
+    "eventUrl": "https://www.facebook.com/events/399616073938964",
+    "venueName": "Atelier Cafe"
+  },
+  {
+    "act": "Chrysta Bell",
+    "date": "2019-04-05",
+    "type": "Concert",
+    "eventUrl": "https://www.facebook.com/events/240114326935956",
+    "venueName": "Form Space"
+  },
+  {
+    "act": "Paramida",
+    "date": "2019-04-05",
+    "type": "DJ Set",
+    "eventUrl": "https://www.facebook.com/events/2274396012608761",
+    "venueName": "Form Space"
+  },
+  {
+    "act": "Prichindel",
+    "date": "2019-04-05",
+    "type": "DJ Set",
+    "eventUrl": "https://www.facebook.com/events/543573479461536",
+    "venueName": "SubForm"
   }
 ]
