b425fc7b5b98eda28fa80c160e4caf7d4ec39452
Andrei Stoicescu <andrei.stoicescu@yopeso.com>
2019-03-28 17:40:44 +0200

    Add canceled type for event

--- data/data.js
+++ data/data.js
@@ -4,209 +4,239 @@ exports.events = [
     "date": "2019-03-28",
     "type": "Vernissage",
     "eventUrl": "https://www.facebook.com/events/161588834749211",
-    "venueName": "White Cuib"
+    "venueName": "White Cuib",
+    "canceled": ""
   },
   {
     "act": "Ted Amber",
     "date": "2019-03-28",
     "type": "DJ Set",
     "eventUrl": "https://www.facebook.com/events/213982196151697",
-    "venueName": "Gazette"
+    "venueName": "Gazette",
+    "canceled": ""
   },
   {
     "act": "Critical Mass",
     "date": "2019-03-29",
     "type": "Gathering",
     "eventUrl": "https://www.facebook.com/events/401313647093262",
-    "venueName": "Piața Unirii"
+    "venueName": "Piața Unirii",
+    "canceled": ""
   },
   {
     "act": "Povești din mileniul trecut",
     "date": "2019-03-29",
     "type": "Installation Art",
     "eventUrl": "https://www.facebook.com/events/2120630374844274",
-    "venueName": "REACTOR de creație și experiment"
+    "venueName": "REACTOR de creație și experiment",
+    "canceled": ""
   },
   {
     "act": "From the belly of a circle comes out a square",
     "date": "2019-03-29",
     "type": "Vernissage",
     "eventUrl": "https://www.facebook.com/events/2349278148662887/",
-    "venueName": "Matca Artspace"
+    "venueName": "Matca Artspace",
+    "canceled": ""
   },
   {
     "act": "Taraf de Caliu",
     "date": "2019-03-29",
     "type": "Concert",
     "eventUrl": "https://www.facebook.com/events/753661408343757",
-    "venueName": "Form Space"
+    "venueName": "Form Space",
+    "canceled": ""
   },
   {
     "act": "Te Doare",
     "date": "2019-03-29",
     "type": "DJ Set",
     "eventUrl": "https://www.facebook.com/events/258691768341759/",
-    "venueName": "Colectiva Gazette"
+    "venueName": "Colectiva Gazette",
+    "canceled": ""
   },
   {
     "act": "Alci",
     "date": "2019-03-29",
     "type": "DJ Set",
     "eventUrl": "https://www.facebook.com/events/2226825930712974",
-    "venueName": "Sequence"
+    "venueName": "Sequence",
+    "canceled": ""
   },
   {
     "act": "Dragos Ilici",
     "date": "2019-03-29",
     "type": "DJ Set",
     "eventUrl": "https://www.facebook.com/events/1998941073537830",
-    "venueName": "Gazette"
+    "venueName": "Gazette",
+    "canceled": ""
   },
   {
     "act": "99,6%",
     "date": "2019-03-30",
     "type": "Theatre",
     "eventUrl": "https://www.facebook.com/events/2200514626876985",
-    "venueName": "REACTOR de creație și experiment"
+    "venueName": "REACTOR de creație și experiment",
+    "canceled": ""
   },
   {
     "act": "Clasa noastră",
     "date": "2019-03-30",
     "type": "Theatre",
     "eventUrl": "https://www.facebook.com/events/1931325953663819",
-    "venueName": "Teatrul Naţional Cluj-Napoca"
+    "venueName": "Teatrul Naţional Cluj-Napoca",
+    "canceled": ""
   },
   {
     "act": "Mihai Viteazul // Tranziții",
     "date": "2019-03-30",
     "type": "Theatre",
     "eventUrl": "https://www.facebook.com/events/364525007728012",
-    "venueName": "Casa Tranzit"
+    "venueName": "Casa Tranzit",
+    "canceled": ""
   },
   {
     "act": "Skyharbor",
     "date": "2019-03-30",
     "type": "Concert",
     "eventUrl": "https://www.facebook.com/events/1051550408365029",
-    "venueName": "Flying Circus"
+    "venueName": "Flying Circus",
+    "canceled": "TRUE"
   },
   {
     "act": "Bondax",
     "date": "2019-03-30",
     "type": "DJ Set",
     "eventUrl": "https://www.facebook.com/events/550531965468174",
-    "venueName": "Sala Polivalentă"
+    "venueName": "Sala Polivalentă",
+    "canceled": ""
   },
   {
     "act": "Nick Curly",
     "date": "2019-03-30",
     "type": "DJ Set",
     "eventUrl": "https://www.facebook.com/events/111699946514565",
-    "venueName": "Form Space"
+    "venueName": "Form Space",
+    "canceled": ""
   },
   {
     "act": "99,6%",
     "date": "2019-03-31",
     "type": "Theatre",
     "eventUrl": "https://www.facebook.com/events/2200514626876985",
-    "venueName": "REACTOR de creație și experiment"
+    "venueName": "REACTOR de creație și experiment",
+    "canceled": ""
   },
   {
     "act": "ZÉRÓ",
     "date": "2019-03-31",
     "type": "Theatre",
     "eventUrl": "https://www.facebook.com/events/2392535474309119",
-    "venueName": "ZUG.zone"
+    "venueName": "ZUG.zone",
+    "canceled": ""
   },
   {
     "act": "Sense",
     "date": "2019-03-31",
     "type": "Concert",
     "eventUrl": "https://www.facebook.com/events/259851024891754",
-    "venueName": "Atelier Cafe"
+    "venueName": "Atelier Cafe",
+    "canceled": ""
   },
   {
     "act": "ZÉRÓ",
     "date": "2019-04-01",
     "type": "Theatre",
     "eventUrl": "https://www.facebook.com/events/508225239583941",
-    "venueName": "ZUG.zone"
+    "venueName": "ZUG.zone",
+    "canceled": ""
   },
   {
     "act": "Nu am un plan B",
     "date": "2019-04-01",
     "type": "Vernissage",
     "eventUrl": "https://www.facebook.com/events/367967574050756/",
-    "venueName": "Atelier Patru"
+    "venueName": "Atelier Patru",
+    "canceled": ""
   },
   {
     "act": "Window",
     "date": "2019-04-04",
     "type": "Concert",
     "eventUrl": "https://www.facebook.com/events/399616073938964",
-    "venueName": "Atelier Cafe"
+    "venueName": "Atelier Cafe",
+    "canceled": ""
   },
   {
     "act": "Chrysta Bell",
     "date": "2019-04-05",
     "type": "Concert",
     "eventUrl": "https://www.facebook.com/events/240114326935956",
-    "venueName": "Form Space"
+    "venueName": "Form Space",
+    "canceled": ""
   },
   {
     "act": "Paramida",
     "date": "2019-04-05",
     "type": "DJ Set",
     "eventUrl": "https://www.facebook.com/events/2274396012608761",
-    "venueName": "Form Space"
+    "venueName": "Form Space",
+    "canceled": ""
   },
   {
     "act": "Prichindel",
     "date": "2019-04-05",
     "type": "DJ Set",
     "eventUrl": "https://www.facebook.com/events/543573479461536",
-    "venueName": "SubForm"
+    "venueName": "SubForm",
+    "canceled": ""
   },
   {
     "act": "Du Levande",
     "date": "2019-04-06",
     "type": "Movie",
     "eventUrl": "https://www.facebook.com/events/399985757222379/",
-    "venueName": "Colectiva Gazette"
+    "venueName": "Colectiva Gazette",
+    "canceled": ""
   },
   {
     "act": "Dungaree",
     "date": "2019-04-10",
     "type": "Concert",
     "eventUrl": "https://www.facebook.com/events/853344068342544/",
-    "venueName": "Flying Circus"
+    "venueName": "Flying Circus",
+    "canceled": ""
   },
   {
     "act": "Space of Variations",
     "date": "2019-04-11",
     "type": "Concert",
     "eventUrl": "https://www.facebook.com/events/391297141670958",
-    "venueName": "Hardward Pub"
+    "venueName": "Hardward Pub",
+    "canceled": ""
   },
   {
     "act": "Matteo Islandezu",
     "date": "2019-04-12",
     "type": "Party",
     "eventUrl": "https://www.facebook.com/events/342602879707171/",
-    "venueName": "Flying Circus"
+    "venueName": "Flying Circus",
+    "canceled": ""
   },
   {
     "act": "Anaid",
     "date": "2019-04-13",
     "type": "DJ Set",
     "eventUrl": "https://www.facebook.com/events/668188046949732/",
-    "venueName": "Cotton"
+    "venueName": "Cotton",
+    "canceled": ""
   },
   {
     "act": "Alandala",
     "date": "2019-04-13",
     "type": "DJ Set",
     "eventUrl": "https://www.facebook.com/events/340469389925861/",
-    "venueName": "Aroma Cluj"
+    "venueName": "Aroma Cluj",
+    "canceled": ""
   }
 ]
