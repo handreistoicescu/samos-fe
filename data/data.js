3fa35d7c8218bd45702d61b1a34691726f6add5d
Andrei Stoicescu <andrei.stoicescu@yopeso.com>
2019-04-02 13:51:25 +0300

    Update events

--- data/data.js
+++ data/data.js
@@ -32,6 +32,22 @@ exports.events = [
 		canceled: ''
 	},
 	{
+		act: 'BAU!',
+		date: '2019-04-06',
+		type: 'Vernissage',
+		eventUrl: 'https://www.facebook.com/events/316694385716539/',
+		venueName: 'Invitro',
+		canceled: ''
+	},
+	{
+		act: 'Marcus Aburelius',
+		date: '2019-04-06',
+		type: 'Concert',
+		eventUrl: 'https://www.facebook.com/events/265051481114846',
+		venueName: 'Gazette',
+		canceled: ''
+	},
+	{
 		act: 'Du Levande',
 		date: '2019-04-06',
 		type: 'Movie',
@@ -56,19 +72,19 @@ exports.events = [
 		canceled: ''
 	},
 	{
-		act: 'Space of Variations',
-		date: '2019-04-11',
-		type: 'Concert',
-		eventUrl: 'https://www.facebook.com/events/391297141670958',
-		venueName: 'Hardward Pub',
-		canceled: ''
-	},
-	{
 		act: 'Lift - boys will be boys',
 		date: '2019-04-11',
 		type: 'Performance Art',
 		eventUrl: 'https://www.facebook.com/events/360372481353428/',
 		venueName: 'Fabrica de Pensule',
+		canceled: ''
+	},
+	{
+		act: 'Flatsharks',
+		date: '2019-04-12',
+		type: 'Concert',
+		eventUrl: 'https://www.facebook.com/events/617917438635139/',
+		venueName: '',
 		canceled: ''
 	},
 	{
@@ -80,11 +96,43 @@ exports.events = [
 		canceled: ''
 	},
 	{
+		act: 'TC80',
+		date: '2019-04-12',
+		type: 'DJ Set',
+		eventUrl: 'https://www.facebook.com/events/2282446078636759/',
+		venueName: 'Gazette',
+		canceled: ''
+	},
+	{
+		act: 'Magdalena',
+		date: '2019-04-12',
+		type: 'DJ Set',
+		eventUrl: 'https://www.facebook.com/events/599287493830437/',
+		venueName: 'Form Space',
+		canceled: ''
+	},
+	{
 		act: 'Alandala',
 		date: '2019-04-13',
 		type: 'DJ Set',
 		eventUrl: 'https://www.facebook.com/events/340469389925861/',
 		venueName: 'Aroma Cluj',
+		canceled: ''
+	},
+	{
+		act: 'Rodion',
+		date: '2019-04-13',
+		type: 'DJ Set',
+		eventUrl: 'https://www.facebook.com/events/348622432414206/',
+		venueName: 'SubForm',
+		canceled: ''
+	},
+	{
+		act: 'Crihan',
+		date: '2019-04-13',
+		type: 'DJ Set',
+		eventUrl: 'https://www.facebook.com/events/2406335719586171/',
+		venueName: 'SubForm',
 		canceled: ''
 	}
 ];
