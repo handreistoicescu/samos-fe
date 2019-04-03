fa1532e2840c538f4b931f468a51af9c154c6dd9
Andrei Stoicescu <andrei.stoicescu@yopeso.com>
2019-04-03 12:14:15 +0300

    Update events

--- data/data.js
+++ data/data.js
@@ -16,6 +16,14 @@ exports.events = [
 		canceled: ''
 	},
 	{
+		act: 'Alexandra Mereuți',
+		date: '2019-04-04',
+		type: 'Vernissage',
+		eventUrl: 'https://www.facebook.com/events/337042190283897/',
+		venueName: 'Quadro 21 Gallery',
+		canceled: ''
+	},
+	{
 		act: 'Window',
 		date: '2019-04-04',
 		type: 'Concert',
@@ -48,7 +56,7 @@ exports.events = [
 		canceled: ''
 	},
 	{
-		act: 'BAU!',
+		act: 'Ștefan Ungureanu',
 		date: '2019-04-06',
 		type: 'Vernissage',
 		eventUrl: 'https://www.facebook.com/events/316694385716539/',
@@ -80,6 +88,14 @@ exports.events = [
 		canceled: ''
 	},
 	{
+		act: "Nora • A Doll's House",
+		date: '2019-04-08',
+		type: 'Performance Art',
+		eventUrl: 'https://www.facebook.com/events/2147368292043156/',
+		venueName: 'Teatrul Maghiar de Stat Cluj',
+		canceled: ''
+	},
+	{
 		act: 'Dungaree',
 		date: '2019-04-10',
 		type: 'Concert',
@@ -96,11 +112,27 @@ exports.events = [
 		canceled: ''
 	},
 	{
+		act: 'Noi nu, niciodată',
+		date: '2019-04-11',
+		type: 'Performance Art',
+		eventUrl: 'https://www.facebook.com/events/316526655729995/',
+		venueName: 'REACTOR de creație și experiment',
+		canceled: ''
+	},
+	{
+		act: 'Love me tinder',
+		date: '2019-04-12',
+		type: 'Performance Art',
+		eventUrl: 'https://www.facebook.com/events/346433852644163/',
+		venueName: 'reACTOR de creație și experiment',
+		canceled: ''
+	},
+	{
 		act: 'Flatsharks',
 		date: '2019-04-12',
 		type: 'Concert',
 		eventUrl: 'https://www.facebook.com/events/617917438635139/',
-		venueName: '',
+		venueName: 'Wine Notes',
 		canceled: ''
 	},
 	{
@@ -128,7 +160,23 @@ exports.events = [
 		canceled: ''
 	},
 	{
-		act: 'Alandala',
+		act: 'Románia 100',
+		date: '2019-04-11',
+		type: 'Performance Art',
+		eventUrl: 'https://www.facebook.com/events/514576809074047/',
+		venueName: 'ZUG.zone',
+		canceled: ''
+	},
+	{
+		act: 'Las Meninas',
+		date: '2019-04-13',
+		type: 'Performance Art',
+		eventUrl: 'https://www.facebook.com/events/465917407573026/',
+		venueName: 'Teatrul Maghiar de Stat Cluj',
+		canceled: ''
+	},
+	{
+		act: 'Maayan Nidam',
 		date: '2019-04-13',
 		type: 'DJ Set',
 		eventUrl: 'https://www.facebook.com/events/340469389925861/',
