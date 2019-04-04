c93af28f78be6fdbf897000017864d70e4c81af3
Drei <stoicescuofsuburbia@gmail.com>
2019-04-04 08:23:56 +0300

    Update events

--- data/data.js
+++ data/data.js
@@ -1,21 +1,5 @@
 exports.events = [
 	{
-		act: 'Pepo Salazar',
-		date: '2019-04-03',
-		type: 'Vernissage',
-		eventUrl: 'https://www.facebook.com/events/1071645389694059/',
-		venueName: 'SABOT',
-		canceled: ''
-	},
-	{
-		act: 'Mihai Plătică',
-		date: '2019-04-03',
-		type: 'Vernissage',
-		eventUrl: 'https://www.facebook.com/events/263808734497463/',
-		venueName: 'Baril',
-		canceled: ''
-	},
-	{
 		act: 'Alexandra Mereuți',
 		date: '2019-04-04',
 		type: 'Vernissage',
