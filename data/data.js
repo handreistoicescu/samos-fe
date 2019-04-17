de7496bcdaa61b7ec1ff9c00108cb24cc414a2cf
Drei <stoicescuofsuburbia@gmail.com>
2019-04-17 08:56:02 +0300

    Update events

--- data/data.js
+++ data/data.js
@@ -1,13 +1,5 @@
 exports.events = [
 	{
-		act: 'Vârsta 4. Pe ritm de Lambada',
-		date: '2019-04-16',
-		type: 'Movie',
-		eventUrl: 'https://www.facebook.com/events/343903709577172/',
-		venueName: 'REACTOR de creație și experiment',
-		canceled: ''
-	},
-	{
 		act: 'WASTED! The Story of Food Waste',
 		date: '2019-04-17',
 		type: 'Movie',
@@ -172,46 +164,6 @@ exports.events = [
 		date: '2019-04-23',
 		type: 'Performance Art',
 		eventUrl: 'https://www.facebook.com/events/576186972856887/',
-		venueName: 'Teatrul Maghiar de Stat Cluj',
-		canceled: ''
-	},
-	{
-		act: 'Zapruder',
-		date: '2019-04-25',
-		type: 'Concert',
-		eventUrl: 'https://www.facebook.com/events/746194879095759/',
-		venueName: 'Urania Palace',
-		canceled: ''
-	},
-	{
-		act: 'Óperentzia',
-		date: '2019-04-25',
-		type: 'Concert',
-		eventUrl: 'https://www.facebook.com/events/2250997314957010/',
-		venueName: 'Gazette',
-		canceled: ''
-	},
-	{
-		act: 'Vulture Industries',
-		date: '2019-04-26',
-		type: 'Concert',
-		eventUrl: 'https://www.facebook.com/events/301977777122769/',
-		venueName: 'Flying Circus',
-		canceled: ''
-	},
-	{
-		act: 'Fishez',
-		date: '2019-04-27',
-		type: 'Performance Art',
-		eventUrl: 'https://www.facebook.com/events/840660082981047/',
-		venueName: 'ZUG.zone',
-		canceled: ''
-	},
-	{
-		act: 'Las Meninas',
-		date: '2019-04-29',
-		type: 'Performance Art',
-		eventUrl: 'https://www.facebook.com/events/465917407573026/',
 		venueName: 'Teatrul Maghiar de Stat Cluj',
 		canceled: ''
 	}
