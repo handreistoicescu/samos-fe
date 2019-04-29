8da6885f1f64728e15cd9ae204fa81dc45792480
Drei <stoicescuofsuburbia@gmail.com>
2019-04-29 22:55:21 +0300

    Update events

--- data/data.js
+++ data/data.js
@@ -1,21 +1,5 @@
 exports.events = [
 	{
-		act: 'Fishez',
-		date: '2019-04-27',
-		type: 'Performance Art',
-		eventUrl: 'https://www.facebook.com/events/840660082981047/',
-		venueName: 'ZUG.zone',
-		canceled: ''
-	},
-	{
-		act: 'Las Meninas',
-		date: '2019-04-29',
-		type: 'Performance Art',
-		eventUrl: 'https://www.facebook.com/events/465917407573026/',
-		venueName: 'Teatrul Maghiar de Stat Cluj',
-		canceled: ''
-	},
-	{
 		act: 'Jonathan Bree',
 		date: '2019-05-04',
 		type: 'Concert',
@@ -45,6 +29,22 @@ exports.events = [
 		type: 'Performance Art',
 		eventUrl: 'https://www.facebook.com/events/299419674304969/',
 		venueName: 'Fabrica de Pensule',
+		canceled: ''
+	},
+	{
+		act: 'C’est bon • E ok • Rendben • This is just a story',
+		date: '2019-05-10',
+		type: 'Performance Art',
+		eventUrl: 'https://www.facebook.com/events/299419674304969/',
+		venueName: 'Fabrica de Pensule',
+		canceled: ''
+	},
+	{
+		act: 'Smallpeople',
+		date: '2019-05-10',
+		type: 'DJ Set',
+		eventUrl: 'https://www.facebook.com/events/2525009004239265/',
+		venueName: 'Gazette',
 		canceled: ''
 	}
 ];
