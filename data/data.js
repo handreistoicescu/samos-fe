3a00af37ee42d5e7896747c5cf64adaa6bab254b
Drei <stoicescuofsuburbia@gmail.com>
2019-05-02 12:59:58 +0300

    Update events

--- data/data.js
+++ data/data.js
@@ -1,10 +1,42 @@
 exports.events = [
 	{
+		act: 'Tincuţa Marin',
+		date: '2019-05-03',
+		type: 'Vernissage',
+		eventUrl: 'https://www.facebook.com/events/1109449009258195/',
+		venueName: 'White Cuib',
+		canceled: ''
+	},
+	{
 		act: 'Jonathan Bree',
 		date: '2019-05-04',
 		type: 'Concert',
 		eventUrl: 'https://www.facebook.com/events/2263043713984275/',
 		venueName: 'Form Space',
+		canceled: ''
+	},
+	{
+		act: 'Sasha',
+		date: '2019-05-04',
+		type: 'DJ Set',
+		eventUrl: 'https://www.facebook.com/events/331372714396491/',
+		venueName: 'Form Space',
+		canceled: ''
+	},
+	{
+		act: 'Solo / group exhibitions',
+		date: '2019-05-07',
+		type: 'Vernissage',
+		eventUrl: 'https://www.facebook.com/events/2255034604557606/',
+		venueName: 'Centrul de Interes',
+		canceled: ''
+	},
+	{
+		act: 'La Plasticatta',
+		date: '2019-05-07',
+		type: 'Performance Art',
+		eventUrl: 'https://www.facebook.com/events/658142427959949/',
+		venueName: 'REACTOR de creație și experiment',
 		canceled: ''
 	},
 	{
@@ -16,9 +48,17 @@ exports.events = [
 		canceled: ''
 	},
 	{
+		act: 'Baladele memoriei',
+		date: '2019-05-08',
+		type: 'Theatre',
+		eventUrl: 'https://www.facebook.com/events/436876860411391/',
+		venueName: 'REACTOR de creație și experiment',
+		canceled: ''
+	},
+	{
 		act: 'Las Meninas',
 		date: '2019-05-08',
-		type: 'Performance Art',
+		type: 'Theatre',
 		eventUrl: 'https://www.facebook.com/events/465917407573026/',
 		venueName: 'Teatrul Maghiar de Stat Cluj',
 		canceled: ''
@@ -45,6 +85,46 @@ exports.events = [
 		type: 'DJ Set',
 		eventUrl: 'https://www.facebook.com/events/2525009004239265/',
 		venueName: 'Gazette',
+		canceled: ''
+	},
+	{
+		act: 'Chaos In The CBD',
+		date: '2019-05-10',
+		type: 'DJ Set',
+		eventUrl: 'https://www.facebook.com/events/405702126645391/',
+		venueName: 'Form Space',
+		canceled: ''
+	},
+	{
+		act: 'Pœtrip',
+		date: '2019-05-11',
+		type: 'Concert',
+		eventUrl: 'https://www.facebook.com/events/433635474082593/',
+		venueName: 'Colectiva Gazette',
+		canceled: ''
+	},
+	{
+		act: 'Lumière',
+		date: '2019-05-11',
+		type: 'DJ Set',
+		eventUrl: 'https://www.facebook.com/events/370423980237438/',
+		venueName: 'Gazette',
+		canceled: ''
+	},
+	{
+		act: 'Soul Glo',
+		date: '2019-05-14',
+		type: 'Concert',
+		eventUrl: 'https://www.facebook.com/events/243328679880914/',
+		venueName: 'Urania Palace',
+		canceled: ''
+	},
+	{
+		act: "Nora • A Doll's House",
+		date: '2019-05-16',
+		type: 'Theatre',
+		eventUrl: 'https://www.facebook.com/events/2147368292043156/',
+		venueName: 'Teatrul Maghiar de Stat Cluj',
 		canceled: ''
 	}
 ];
