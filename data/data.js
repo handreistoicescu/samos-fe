1a4ca4689a66f93cc0313c9922c820be86c235bb
Drei <stoicescuofsuburbia@gmail.com>
2019-05-16 18:00:14 +0300

    Update events

--- data/data.js
+++ data/data.js
@@ -24,6 +24,22 @@ exports.events = [
 		canceled: ''
 	},
 	{
+		act: 'Los Bastoneros',
+		date: '2019-05-17',
+		type: 'DJ Set',
+		eventUrl: 'https://www.facebook.com/events/464195047654473/',
+		venueName: 'Gazette',
+		canceled: ''
+	},
+	{
+		act: 'Underground in the 80s - Deutschland-Romania',
+		date: '2019-05-17',
+		type: 'Vernissage',
+		eventUrl: 'https://www.facebook.com/events/437078036864363/',
+		venueName: 'H33',
+		canceled: ''
+	},
+	{
 		act: 'Ditzner Twintett',
 		date: '2019-05-18',
 		type: 'Concert',
@@ -40,11 +56,83 @@ exports.events = [
 		canceled: ''
 	},
 	{
+		act: 'Kosta',
+		date: '2019-05-18',
+		type: 'DJ Set',
+		eventUrl: 'https://www.facebook.com/events/438512400246281/',
+		venueName: 'Gazette',
+		canceled: ''
+	},
+	{
+		act: 'Middlemist Red',
+		date: '2019-05-18',
+		type: 'Concert',
+		eventUrl: 'https://www.facebook.com/events/1772034009564142/',
+		venueName: 'Atelier Cafe',
+		canceled: ''
+	},
+	{
+		act: 'Ditzner Twintett',
+		date: '2019-05-18',
+		type: 'Ciné Concert',
+		eventUrl: 'https://www.facebook.com/events/315031169183001/',
+		venueName: 'Casa Tranzit',
+		canceled: ''
+	},
+	{
+		act: 'Mircea Kiraly',
+		date: '2019-05-18',
+		type: 'Concert',
+		eventUrl: 'https://www.facebook.com/events/450581779042074/',
+		venueName: 'Colectiva Gazette',
+		canceled: ''
+	},
+	{
 		act: 'Alex Harding',
 		date: '2019-05-21',
 		type: 'Concert',
 		eventUrl: 'https://www.facebook.com/events/428265471279237/',
 		venueName: 'Teatrul Maghiar de Stat Cluj',
+		canceled: ''
+	},
+	{
+		act: 'Laurențiu Ruță',
+		date: '2019-05-23',
+		type: 'Vernissage',
+		eventUrl: 'https://www.facebook.com/events/283660529251049/',
+		venueName: 'Quadro 21 Gallery',
+		canceled: ''
+	},
+	{
+		act: 'Paddy and The Rats',
+		date: '2019-05-23',
+		type: 'Concert',
+		eventUrl: 'https://www.facebook.com/events/711362022593708/',
+		venueName: 'Flying Circus',
+		canceled: ''
+	},
+	{
+		act: 'Expoziție de muzică',
+		date: '2019-05-24',
+		type: 'Performance Art',
+		eventUrl: 'https://www.facebook.com/events/320773101953251/',
+		venueName: 'Invitro',
+		canceled: ''
+	},
+	{
+		act: 'Voiron',
+		date: '2019-05-24',
+		type: 'DJ Set',
+		eventUrl: 'https://www.facebook.com/events/421629391721375/',
+		venueName: 'Gazette',
+		canceled: ''
+	},
+	{
+		act: 'Critica socială și artistică a capitalismului românesc',
+		date: '2019-05-25',
+		type: 'Debate',
+		eventUrl: 'https://www.facebook.com/events/1225815240906244/',
+		venueName: 'Facultatea de Sociologie și Asistență Socială',
 		canceled: ''
 	},
 	{
@@ -56,11 +144,51 @@ exports.events = [
 		canceled: ''
 	},
 	{
+		act: 'Henry Saiz',
+		date: '2019-05-25',
+		type: 'Concert',
+		eventUrl: 'https://www.facebook.com/events/539432336584074/',
+		venueName: 'Casino Park',
+		canceled: ''
+	},
+	{
+		act: 'Andrey Pushkarev',
+		date: '2019-05-25',
+		type: 'DJ Set',
+		eventUrl: 'https://www.facebook.com/events/539432336584074/',
+		venueName: 'BT Arena',
+		canceled: ''
+	},
+	{
 		act: 'Las Meninas',
 		date: '2019-05-27',
 		type: 'Performance Art',
 		eventUrl: 'https://www.facebook.com/events/465917407573026/',
 		venueName: 'Teatrul Maghiar de Stat Cluj',
+		canceled: ''
+	},
+	{
+		act: 'Europeana',
+		date: '2019-05-27',
+		type: 'Theatre',
+		eventUrl: 'https://www.facebook.com/events/2313019665688080/',
+		venueName: 'Teatrul Naţional Cluj-Napoca',
+		canceled: ''
+	},
+	{
+		act: 'Europeana',
+		date: '2019-05-27',
+		type: 'Theatre',
+		eventUrl: 'https://www.facebook.com/events/2313019665688080/',
+		venueName: 'Teatrul Naţional Cluj-Napoca',
+		canceled: ''
+	},
+	{
+		act: 'VAR',
+		date: '2019-05-28',
+		type: 'Concert',
+		eventUrl: 'https://www.facebook.com/events/406606590120625/',
+		venueName: 'SubForm',
 		canceled: ''
 	},
 	{
