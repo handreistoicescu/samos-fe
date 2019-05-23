17de534c46e0a119aee15652a2c86f01f7219d3f
Drei <stoicescuofsuburbia@gmail.com>
2019-05-23 17:24:42 +0300

    Update events

--- data/data.js
+++ data/data.js
@@ -1,5 +1,13 @@
 exports.events = [
 	{
+		act: 'George Rosu',
+		date: '2019-05-23',
+		type: 'Vernissage',
+		eventUrl: 'https://www.facebook.com/events/2219452544815129/',
+		venueName: 'White Cuib',
+		canceled: ''
+	},
+	{
 		act: 'Laurențiu Ruță',
 		date: '2019-05-23',
 		type: 'Vernissage',
@@ -16,9 +24,25 @@ exports.events = [
 		canceled: ''
 	},
 	{
-		act: 'Expoziție de muzică',
+		act: 'Alexandra Mocan și Camilia Filipov',
 		date: '2019-05-24',
-		type: 'Performance Art',
+		type: 'Vernissage',
+		eventUrl: 'https://www.facebook.com/events/356624115201380/',
+		venueName: 'Atelier Patru',
+		canceled: ''
+	},
+	{
+		act: 'Bucurie și fericire',
+		date: '2019-05-24',
+		type: 'Theatre',
+		eventUrl: 'https://www.facebook.com/events/431249207652625/',
+		venueName: 'Casa Tranzit',
+		canceled: ''
+	},
+	{
+		act: 'Zănatec',
+		date: '2019-05-24',
+		type: 'Concert',
 		eventUrl: 'https://www.facebook.com/events/320773101953251/',
 		venueName: 'Invitro',
 		canceled: ''
@@ -32,11 +56,19 @@ exports.events = [
 		canceled: ''
 	},
 	{
-		act: 'Critica socială și artistică a capitalismului românesc',
+		act: 'Enikő Vincze',
 		date: '2019-05-25',
 		type: 'Debate',
 		eventUrl: 'https://www.facebook.com/events/1225815240906244/',
 		venueName: 'Facultatea de Sociologie și Asistență Socială',
+		canceled: ''
+	},
+	{
+		act: 'Francesco Arecco',
+		date: '2019-05-25',
+		type: 'Vernissage',
+		eventUrl: 'https://www.facebook.com/events/2369382820052954/',
+		venueName: 'IAGA ContemporaryArt',
 		canceled: ''
 	},
 	{
@@ -64,6 +96,14 @@ exports.events = [
 		canceled: ''
 	},
 	{
+		act: 'Miracolul de la Cluj',
+		date: '2019-05-26',
+		type: 'Theatre',
+		eventUrl: 'https://www.facebook.com/events/2719168754824815/',
+		venueName: 'REACTOR de creație și experiment',
+		canceled: ''
+	},
+	{
 		act: 'Las Meninas',
 		date: '2019-05-27',
 		type: 'Performance Art',
@@ -72,19 +112,27 @@ exports.events = [
 		canceled: ''
 	},
 	{
-		act: 'Europeana',
-		date: '2019-05-27',
-		type: 'Theatre',
-		eventUrl: 'https://www.facebook.com/events/2313019665688080/',
-		venueName: 'Teatrul Naţional Cluj-Napoca',
+		act: 'Taietzel Ticalos',
+		date: '2019-05-28',
+		type: 'Vernissage',
+		eventUrl: '',
+		venueName: 'Str. Horea nr. 5, ap. 22',
 		canceled: ''
 	},
 	{
 		act: 'Europeana',
-		date: '2019-05-27',
+		date: '2019-05-28',
 		type: 'Theatre',
-		eventUrl: 'https://www.facebook.com/events/2313019665688080/',
+		eventUrl: 'https://www.facebook.com/events/2275453599386593/',
 		venueName: 'Teatrul Naţional Cluj-Napoca',
+		canceled: ''
+	},
+	{
+		act: 'Love me tinder',
+		date: '2019-05-28',
+		type: 'Theatre',
+		eventUrl: 'https://www.facebook.com/events/391142385062302/',
+		venueName: 'REACTOR de creație și experiment',
 		canceled: ''
 	},
 	{
@@ -96,11 +144,43 @@ exports.events = [
 		canceled: ''
 	},
 	{
+		act: 'Banda',
+		date: '2019-05-29',
+		type: 'Theatre',
+		eventUrl: 'https://www.facebook.com/events/2641889489178950/',
+		venueName: 'REACTOR de creație și experiment',
+		canceled: ''
+	},
+	{
 		act: 'Unpredicted',
 		date: '2019-05-30',
 		type: 'Theatre',
 		eventUrl: 'https://www.facebook.com/events/365507050729942/',
 		venueName: 'ZUG.zone',
+		canceled: ''
+	},
+	{
+		act: 'Critical Mass',
+		date: '2019-05-31',
+		type: 'Gathering',
+		eventUrl: '',
+		venueName: 'Piata Unirii',
+		canceled: ''
+	},
+	{
+		act: 'BabyBlues',
+		date: '2019-06-01',
+		type: 'Theatre',
+		eventUrl: 'https://www.facebook.com/events/309018230026605/',
+		venueName: 'REACTOR de creație și experiment',
+		canceled: ''
+	},
+	{
+		act: 'Mastemey',
+		date: '2019-06-01',
+		type: 'Concert',
+		eventUrl: 'https://www.facebook.com/events/259278748188478/',
+		venueName: 'Hardward Pub',
 		canceled: ''
 	},
 	{
@@ -128,11 +208,27 @@ exports.events = [
 		canceled: ''
 	},
 	{
+		act: '2META+',
+		date: '2019-06-02',
+		type: 'Vernissage',
+		eventUrl: 'https://www.facebook.com/events/295183258029327/',
+		venueName: 'La Cave',
+		canceled: ''
+	},
+	{
 		act: 'Elder',
 		date: '2019-06-03',
 		type: 'Concert',
 		eventUrl: 'https://www.facebook.com/events/2197189450498041/',
 		venueName: 'Flying Circus',
+		canceled: ''
+	},
+	{
+		act: 'Crüel Night',
+		date: '2019-06-04',
+		type: 'Concert',
+		eventUrl: 'https://www.facebook.com/events/308722616483072/',
+		venueName: 'Urania Palace',
 		canceled: ''
 	},
 	{
