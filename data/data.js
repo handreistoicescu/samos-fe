b21978b07139fa13ff489585aa23e29ce257b77a
Drei <stoicescuofsuburbia@gmail.com>
2019-06-08 17:22:48 +0300

    Update events

--- data/data.js
+++ data/data.js
@@ -17,6 +17,14 @@ exports.events = [
 		canceled: ''
 	},
 	{
+		act: 'Baladele memoriei',
+		date: '2019-06-09',
+		type: 'Theatre',
+		eventUrl: 'https://www.facebook.com/events/443055066482159/',
+		venueName: 'REACTOR de creație și experiment',
+		canceled: ''
+	},
+	{
 		act: 'Cold Case Hammarskjöld',
 		date: '2019-06-09',
 		type: 'Documentary',
@@ -44,11 +52,59 @@ exports.events = [
 		canceled: ''
 	},
 	{
+		act: '1000mods',
+		date: '2019-06-09',
+		type: 'Concert',
+		eventUrl: 'https://www.facebook.com/events/834344863625177/',
+		venueName: 'Form Space',
+		canceled: ''
+	},
+	{
+		act: 'Eu sunt! Și?',
+		date: '2019-06-10',
+		type: 'Performance Art',
+		eventUrl: 'https://www.facebook.com/events/443469196217879/',
+		venueName: 'Casa Tranzit',
+		canceled: ''
+	},
+	{
+		act: 'Being OKEY – when love is treated as a crime',
+		date: '2019-06-11',
+		type: 'Documentary',
+		eventUrl: 'https://www.facebook.com/events/322716128425150/',
+		venueName: 'Casa Tranzit',
+		canceled: ''
+	},
+	{
+		act: '99,6%',
+		date: '2019-06-11',
+		type: 'Performance Art',
+		eventUrl: 'https://www.facebook.com/events/448235789310725/',
+		venueName: 'ZUG.zone',
+		canceled: ''
+	},
+	{
+		act: 'Cristina Lilienfeld',
+		date: '2019-06-12',
+		type: 'Performance Art',
+		eventUrl: 'https://www.facebook.com/events/845306795855393/',
+		venueName: 'Fabrica de Pensule',
+		canceled: ''
+	},
+	{
 		act: 'Cannibal Corpse',
 		date: '2019-06-12',
 		type: 'Concert',
 		eventUrl: 'https://www.facebook.com/events/2315571748502218/',
 		venueName: 'Form Space',
+		canceled: ''
+	},
+	{
+		act: 'Emilian Mocanu',
+		date: '2019-06-13',
+		type: 'Vernissage',
+		eventUrl: 'https://www.facebook.com/events/348428515862479/',
+		venueName: 'Atelier Patru',
 		canceled: ''
 	},
 	{
@@ -60,11 +116,99 @@ exports.events = [
 		canceled: ''
 	},
 	{
-		act: 'Dodo',
+		act: 'Fishez',
+		date: '2019-06-13',
+		type: 'Performance Art',
+		eventUrl: 'https://www.facebook.com/events/1059122394291501/',
+		venueName: 'ZUG.zone',
+		canceled: ''
+	},
+	{
+		act: 'Martorii lui Putin',
+		date: '2019-06-14',
+		type: 'Movie',
+		eventUrl: 'https://www.facebook.com/events/380413245902048/',
+		venueName: 'Fabrica de Pensule',
+		canceled: ''
+	},
+	{
+		act: 'Niboowin',
+		date: '2019-06-14',
+		type: 'Concert',
+		eventUrl: 'https://www.facebook.com/events/1110752449132390/',
+		venueName: 'Urania Palace',
+		canceled: ''
+	},
+	{
+		act: 'JFB',
 		date: '2019-06-14',
 		type: 'DJ Set',
-		eventUrl: 'https://www.facebook.com/events/2447051932013083/',
-		venueName: 'Planetarium',
+		eventUrl: 'https://www.facebook.com/events/1158153994366003/',
+		venueName: 'Muzeul de Artă Cluj-Napoca',
+		canceled: ''
+	},
+	{
+		act: 'Olga Korol',
+		date: '2019-06-14',
+		type: 'DJ Set',
+		eventUrl: 'https://www.facebook.com/events/611333876051575',
+		venueName: 'Club Midi',
+		canceled: ''
+	},
+	{
+		act: 'RIEN, mais la j’ai envie de mourir',
+		date: '2019-06-15',
+		type: 'Performance Art',
+		eventUrl: 'https://www.facebook.com/events/366808637276277/',
+		venueName: 'Casa Tranzit',
+		canceled: ''
+	},
+	{
+		act: 'Angata',
+		date: '2019-06-15',
+		type: 'Concert',
+		eventUrl: 'https://www.facebook.com/events/329167527771291/',
+		venueName: 'Dealul Steluța',
+		canceled: ''
+	},
+	{
+		act: 'RPR Soundsystem',
+		date: '2019-06-15',
+		type: 'DJ Set',
+		eventUrl: 'https://www.facebook.com/events/611333876051575',
+		venueName: 'Club Midi',
+		canceled: ''
+	},
+	{
+		act: 'Iona',
+		date: '2019-06-16',
+		type: 'Performance Art',
+		eventUrl: 'https://www.facebook.com/events/295302604709181/',
+		venueName: 'Teatrul Maghiar de Stat Cluj',
+		canceled: ''
+	},
+	{
+		act: "Nora • A Doll's House",
+		date: '2019-06-18',
+		type: 'Performance Art',
+		eventUrl: 'https://www.facebook.com/events/2147368292043156/',
+		venueName: 'Teatrul Maghiar de Stat Cluj',
+		canceled: ''
+	},
+	{
+		act: 'Ingested',
+		date: '2019-06-19',
+		type: 'Concert',
+		eventUrl: 'https://www.facebook.com/events/2282404315160688/',
+		venueName: 'Flying Circus',
+		canceled: ''
+	},
+	{
+		act: 'Nionde Plågan',
+		date: '2019-06-23',
+		type: 'Concert',
+		eventUrl: 'https://www.facebook.com/events/539255416562561/',
+		venueName: 'Urania Palace',
 		canceled: ''
 	}
 ];
