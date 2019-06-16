75a07e81ae1ed9ad763d6e1519fb26d664aee8db
Drei <stoicescuofsuburbia@gmail.com>
2019-06-16 08:16:37 +0300

    Update events

--- data/data.js
+++ data/data.js
@@ -1,45 +1,5 @@
 exports.events = [
 	{
-		act: 'Angata',
-		date: '2019-06-15',
-		type: 'Concert',
-		eventUrl: 'https://www.facebook.com/events/329167527771291/',
-		venueName: 'Dealul Steluța',
-		canceled: ''
-	},
-	{
-		act: 'RPR Soundsystem',
-		date: '2019-06-15',
-		type: 'DJ Set',
-		eventUrl: 'https://www.facebook.com/events/611333876051575',
-		venueName: 'Club Midi',
-		canceled: ''
-	},
-	{
-		act: 'RIEN, mais la j’ai envie de mourir',
-		date: '2019-06-15',
-		type: 'Theatre',
-		eventUrl: 'https://www.facebook.com/events/366808637276277/',
-		venueName: 'Casa Tranzit',
-		canceled: ''
-	},
-	{
-		act: 'Hearts Beat Loud',
-		date: '2019-06-15',
-		type: 'Movie',
-		eventUrl: 'https://www.facebook.com/events/2446800448716432/',
-		venueName: 'Colectiva Gazette',
-		canceled: ''
-	},
-	{
-		act: 'Cluj Pride',
-		date: '2019-06-15',
-		type: 'Gathering',
-		eventUrl: 'https://www.facebook.com/events/284228679125641/',
-		venueName: 'Piața Unirii',
-		canceled: ''
-	},
-	{
 		act: 'Iona',
 		date: '2019-06-16',
 		type: 'Performance Art',
