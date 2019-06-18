b2d85c91234d7a493f15883c931a9c5a4beafd6a
Drei <stoicescuofsuburbia@gmail.com>
2019-06-18 17:32:26 +0300

    Update events

--- data/data.js
+++ data/data.js
@@ -2,7 +2,7 @@ exports.events = [
 	{
 		act: "Nora • A Doll's House",
 		date: '2019-06-18',
-		type: 'Performance Art',
+		type: 'Theatre',
 		eventUrl: 'https://www.facebook.com/events/2147368292043156/',
 		venueName: 'Teatrul Maghiar de Stat Cluj',
 		canceled: ''
@@ -32,6 +32,46 @@ exports.events = [
 		canceled: ''
 	},
 	{
+		act: 'I am here still',
+		date: '2019-06-21',
+		type: 'Performance Art',
+		eventUrl: 'https://www.facebook.com/events/1434259196713961/',
+		venueName: 'Fabrica de Pensule',
+		canceled: ''
+	},
+	{
+		act: 'Fata din Vestul Sălbatic',
+		date: '2019-06-21',
+		type: 'Concert',
+		eventUrl: 'https://www.facebook.com/events/594777420929988/',
+		venueName: 'Filarmonica de Stat Transilvania',
+		canceled: ''
+	},
+	{
+		act: 'Vass',
+		date: '2019-06-21',
+		type: 'DJ Set',
+		eventUrl: 'https://www.facebook.com/events/454094015135204/',
+		venueName: 'Gazette',
+		canceled: ''
+	},
+	{
+		act: 'Glorie',
+		date: '2019-06-22',
+		type: 'Theatre',
+		eventUrl: 'https://www.facebook.com/events/482224092348703/',
+		venueName: 'REACTOR de creație și experiment',
+		canceled: ''
+	},
+	{
+		act: 'Glorie',
+		date: '2019-06-23',
+		type: 'Theatre',
+		eventUrl: 'https://www.facebook.com/events/675210389586988/',
+		venueName: 'REACTOR de creație și experiment',
+		canceled: ''
+	},
+	{
 		act: 'Nionde Plågan',
 		date: '2019-06-23',
 		type: 'Concert',
@@ -40,11 +80,27 @@ exports.events = [
 		canceled: ''
 	},
 	{
+		act: 'Rândul 3, aproape de margine',
+		date: '2019-06-26',
+		type: 'Theatre',
+		eventUrl: 'https://www.facebook.com/events/454990011996573/',
+		venueName: 'REACTOR de creație și experiment',
+		canceled: ''
+	},
+	{
 		act: 'C’est bon • E ok • Rendben • This is just a story',
 		date: '2019-06-27',
 		type: 'Performance Art',
 		eventUrl: 'https://www.facebook.com/events/464846487605527/',
 		venueName: 'Fabrica de Pensule',
+		canceled: ''
+	},
+	{
+		act: 'La Plasticatta',
+		date: '2019-06-27',
+		type: 'Theatre',
+		eventUrl: 'https://www.facebook.com/events/626112744553565/',
+		venueName: 'REACTOR de creație și experiment',
 		canceled: ''
 	}
 ];
