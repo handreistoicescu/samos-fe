671f183074d3c2504995bfe2d9d0ed41d316ff7f
Drei <stoicescuofsuburbia@gmail.com>
2019-07-07 23:28:51 +0300

    Update events

--- data/data.js
+++ data/data.js
@@ -1,53 +1,5 @@
 exports.events = [
 	{
-		act: 'Chassol',
-		date: '2019-07-06',
-		type: 'Concert',
-		eventUrl: 'https://www.jazzinthepark.ro/lineup/chassol/',
-		venueName: 'Parcul Central',
-		canceled: ''
-	},
-	{
-		act: 'Ron Minis',
-		date: '2019-07-06',
-		type: 'Concert',
-		eventUrl: 'https://www.jazzinthepark.ro/lineup/ron-minis/',
-		venueName: 'Parcul Central',
-		canceled: ''
-	},
-	{
-		act: 'Artrax',
-		date: '2019-07-06',
-		type: 'DJ Set',
-		eventUrl: 'https://www.facebook.com/events/591651991344739/',
-		venueName: 'Form Space',
-		canceled: ''
-	},
-	{
-		act: 'Louis Habryn',
-		date: '2019-07-06',
-		type: 'DJ Set',
-		eventUrl: 'https://www.facebook.com/events/421557398687609/',
-		venueName: 'Gazette',
-		canceled: ''
-	},
-	{
-		act: 'Delvon Lamarr Organ Trio',
-		date: '2019-07-07',
-		type: 'Concert',
-		eventUrl: 'https://www.jazzinthepark.ro/lineup/delvon-lamarr-organ-trio/',
-		venueName: 'Parcul Central',
-		canceled: ''
-	},
-	{
-		act: 'Pianohooligan',
-		date: '2019-07-07',
-		type: 'Concert',
-		eventUrl: 'https://www.jazzinthepark.ro/lineup/pianohooligan/',
-		venueName: 'Parcul Central',
-		canceled: ''
-	},
-	{
 		act: 'Enter Shikari',
 		date: '2019-07-10',
 		type: 'Concert',
