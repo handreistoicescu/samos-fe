269e3f5686df72bb24057ba860f24e57fce58d6c
Drei <stoicescuofsuburbia@gmail.com>
2019-07-10 18:25:04 +0300

    Update events

--- data/data.js
+++ data/data.js
@@ -42,7 +42,7 @@ exports.events = [
 	{
 		act: 'Elderbrook',
 		date: '2019-07-17',
-		type: 'Concert',
+		type: 'Live Set',
 		eventUrl: 'https://electriccastle.ro/daily-schedule/',
 		venueName: 'Bonțida',
 		canceled: ''
@@ -50,6 +50,150 @@ exports.events = [
 	{
 		act: 'Made from Scratch',
 		date: '2019-07-17',
+		type: 'DJ Set',
+		eventUrl: 'https://electriccastle.ro/daily-schedule/',
+		venueName: 'Bonțida',
+		canceled: ''
+	},
+	{
+		act: 'The Shapeshifters',
+		date: '2019-07-18',
+		type: 'DJ Set',
+		eventUrl: 'https://electriccastle.ro/daily-schedule/',
+		venueName: 'Bonțida',
+		canceled: ''
+	},
+	{
+		act: 'Gang of Youths',
+		date: '2019-07-18',
+		type: 'Concert',
+		eventUrl: 'https://electriccastle.ro/daily-schedule/',
+		venueName: 'Bonțida',
+		canceled: ''
+	},
+	{
+		act: 'MONO',
+		date: '2019-07-18',
+		type: 'Concert',
+		eventUrl: 'https://electriccastle.ro/daily-schedule/',
+		venueName: 'Bonțida',
+		canceled: ''
+	},
+	{
+		act: 'DELAPORTE',
+		date: '2019-07-18',
+		type: 'Concert',
+		eventUrl: 'https://electriccastle.ro/daily-schedule/',
+		venueName: 'Bonțida',
+		canceled: ''
+	},
+	{
+		act: 'Lamb',
+		date: '2019-07-18',
+		type: 'Concert',
+		eventUrl: 'https://electriccastle.ro/daily-schedule/',
+		venueName: 'Bonțida',
+		canceled: ''
+	},
+	{
+		act: 'Glass Museum',
+		date: '2019-07-18',
+		type: 'Concert',
+		eventUrl: 'https://electriccastle.ro/daily-schedule/',
+		venueName: 'Bonțida',
+		canceled: ''
+	},
+	{
+		act: 'Rone',
+		date: '2019-07-18',
+		type: 'Live Set',
+		eventUrl: 'https://electriccastle.ro/daily-schedule/',
+		venueName: 'Bonțida',
+		canceled: ''
+	},
+	{
+		act: 'Onra',
+		date: '2019-07-18',
+		type: 'DJ Set',
+		eventUrl: 'https://electriccastle.ro/daily-schedule/',
+		venueName: 'Bonțida',
+		canceled: ''
+	},
+	{
+		act: 'Panonia Allstars Ska Orchestra',
+		date: '2019-07-19',
+		type: 'Concert',
+		eventUrl: 'https://electriccastle.ro/daily-schedule/',
+		venueName: 'Bonțida',
+		canceled: ''
+	},
+	{
+		act: 'The Vaccines',
+		date: '2019-07-19',
+		type: 'Concert',
+		eventUrl: 'https://electriccastle.ro/daily-schedule/',
+		venueName: 'Bonțida',
+		canceled: ''
+	},
+	{
+		act: 'Chinese Man',
+		date: '2019-07-19',
+		type: 'DJ Set',
+		eventUrl: 'https://electriccastle.ro/daily-schedule/',
+		venueName: 'Bonțida',
+		canceled: ''
+	},
+	{
+		act: 'Iris Gold',
+		date: '2019-07-19',
+		type: 'Concert',
+		eventUrl: 'https://electriccastle.ro/daily-schedule/',
+		venueName: 'Bonțida',
+		canceled: ''
+	},
+	{
+		act: 'Florence + The Machine',
+		date: '2019-07-19',
+		type: 'Concert',
+		eventUrl: 'https://electriccastle.ro/daily-schedule/',
+		venueName: 'Bonțida',
+		canceled: ''
+	},
+	{
+		act: 'Tommy Cash',
+		date: '2019-07-19',
+		type: 'Concert',
+		eventUrl: 'https://electriccastle.ro/daily-schedule/',
+		venueName: 'Bonțida',
+		canceled: ''
+	},
+	{
+		act: 'Nightmares on Wax',
+		date: '2019-07-19',
+		type: 'DJ Set',
+		eventUrl: 'https://electriccastle.ro/daily-schedule/',
+		venueName: 'Bonțida',
+		canceled: ''
+	},
+	{
+		act: 'Boys Noize',
+		date: '2019-07-19',
+		type: 'DJ Set',
+		eventUrl: 'https://electriccastle.ro/daily-schedule/',
+		venueName: 'Bonțida',
+		canceled: ''
+	},
+	{
+		act: 'The Black Madonna',
+		date: '2019-07-19',
+		type: 'DJ Set',
+		eventUrl: 'https://electriccastle.ro/daily-schedule/',
+		venueName: 'Bonțida',
+		canceled: ''
+	},
+	{
+		act: 'Sama',
+		date: '2019-07-19',
 		type: 'DJ Set',
 		eventUrl: 'https://electriccastle.ro/daily-schedule/',
 		venueName: 'Bonțida',
