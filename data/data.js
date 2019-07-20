680b3a7ca54fa5cb03c3295daa7a7348c5261364
Drei <stoicescuofsuburbia@gmail.com>
2019-07-20 10:05:23 +0300

    Update events

--- data/data.js
+++ data/data.js
@@ -1,89 +1,10 @@
 exports.events = [
 	{
-		act: 'Gorgasm',
-		date: '2019-07-19',
-		type: 'Concert',
-		eventUrl: 'https://www.facebook.com/events/243741589835688/',
-		venueName: 'Flying Circus',
-		canceled: ''
-	},
-	{
-		act: 'Panonia Allstars Ska Orchestra',
-		date: '2019-07-19',
-		type: 'Concert',
-		eventUrl: 'https://electriccastle.ro/schedule',
-		venueName: 'Bonțida',
-		canceled: ''
-	},
-	{
-		act: 'The Vaccines',
-		date: '2019-07-19',
-		type: 'Concert',
-		eventUrl: 'https://electriccastle.ro/schedule',
-		venueName: 'Bonțida',
-		canceled: ''
-	},
-	{
-		act: 'Chinese Man',
-		date: '2019-07-19',
-		type: 'DJ Set',
-		eventUrl: 'https://electriccastle.ro/schedule',
-		venueName: 'Bonțida',
-		canceled: ''
-	},
-	{
-		act: 'Iris Gold',
-		date: '2019-07-19',
-		type: 'Concert',
-		eventUrl: 'https://electriccastle.ro/schedule',
-		venueName: 'Bonțida',
-		canceled: ''
-	},
-	{
-		act: 'Florence + The Machine',
-		date: '2019-07-19',
-		type: 'Concert',
-		eventUrl: 'https://electriccastle.ro/schedule',
-		venueName: 'Bonțida',
-		canceled: ''
-	},
-	{
-		act: 'Tommy Cash',
-		date: '2019-07-19',
-		type: 'Concert',
-		eventUrl: 'https://electriccastle.ro/schedule',
-		venueName: 'Bonțida',
-		canceled: ''
-	},
-	{
-		act: 'Nightmares on Wax',
-		date: '2019-07-19',
-		type: 'DJ Set',
-		eventUrl: 'https://electriccastle.ro/schedule',
-		venueName: 'Bonțida',
-		canceled: ''
-	},
-	{
-		act: 'Boys Noize',
-		date: '2019-07-19',
-		type: 'DJ Set',
-		eventUrl: 'https://electriccastle.ro/schedule',
-		venueName: 'Bonțida',
-		canceled: ''
-	},
-	{
-		act: 'The Black Madonna',
-		date: '2019-07-19',
-		type: 'DJ Set',
-		eventUrl: 'https://electriccastle.ro/schedule',
-		venueName: 'Bonțida',
-		canceled: ''
-	},
-	{
-		act: 'Sama',
-		date: '2019-07-19',
-		type: 'DJ Set',
-		eventUrl: 'https://electriccastle.ro/schedule',
+		act:
+			'Samoș notice: due to the recent racist comments expressed by the organizers of the Electric Castle festival, we at Samos.ro have taken the decision of stopping the promotion of all events related to the aforementioned festival. We strongly encourage you to boycott the rest of the festival and redirect any funds you were intending to spend there to one of the various NGOs that support the Roma community.',
+		date: '2019-07-20',
+		type: 'Various concerts',
+		eventUrl: 'https://romanicriss.org/',
 		venueName: 'Bonțida',
 		canceled: ''
 	},
@@ -96,83 +17,11 @@ exports.events = [
 		canceled: ''
 	},
 	{
-		act: 'Zimbru',
+		act: 'Louis C.K.',
 		date: '2019-07-20',
-		type: 'Concert',
-		eventUrl: 'https://electriccastle.ro/schedule',
-		venueName: 'Bonțida',
-		canceled: ''
-	},
-	{
-		act: 'Valeria Stoica',
-		date: '2019-07-20',
-		type: 'Concert',
-		eventUrl: 'https://electriccastle.ro/schedule',
-		venueName: 'Bonțida',
-		canceled: ''
-	},
-	{
-		act: 'While She Sleeps',
-		date: '2019-07-20',
-		type: 'Concert',
-		eventUrl: 'https://electriccastle.ro/schedule',
-		venueName: 'Bonțida',
-		canceled: ''
-	},
-	{
-		act: 'Nils Frahm',
-		date: '2019-07-20',
-		type: 'Live Set',
-		eventUrl: 'https://electriccastle.ro/schedule',
-		venueName: 'Bonțida',
-		canceled: ''
-	},
-	{
-		act: 'PowerSolo',
-		date: '2019-07-20',
-		type: 'Concert',
-		eventUrl: 'https://electriccastle.ro/schedule',
-		venueName: 'Bonțida',
-		canceled: ''
-	},
-	{
-		act: 'Kronos Quartet',
-		date: '2019-07-20',
-		type: 'Concert',
-		eventUrl: 'https://electriccastle.ro/schedule',
-		venueName: 'Bonțida',
-		canceled: ''
-	},
-	{
-		act: 'Leifur James',
-		date: '2019-07-20',
-		type: 'Live Set',
-		eventUrl: 'https://electriccastle.ro/schedule',
-		venueName: 'Bonțida',
-		canceled: ''
-	},
-	{
-		act: 'Lemaitre',
-		date: '2019-07-20',
-		type: 'Live Set',
-		eventUrl: 'https://electriccastle.ro/schedule',
-		venueName: 'Bonțida',
-		canceled: ''
-	},
-	{
-		act: 'Natasha Diggs',
-		date: '2019-07-20',
-		type: 'DJ Set',
-		eventUrl: 'https://electriccastle.ro/schedule',
-		venueName: 'Bonțida',
-		canceled: ''
-	},
-	{
-		act: 'Marc Hype',
-		date: '2019-07-20',
-		type: 'DJ Set',
-		eventUrl: 'https://electriccastle.ro/schedule',
-		venueName: 'Bonțida',
+		type: 'Stand-up Comedy',
+		eventUrl: 'https://www.facebook.com/events/196352844644962/',
+		venueName: 'Casa de Cultură a Studenților',
 		canceled: ''
 	},
 	{
@@ -189,94 +38,6 @@ exports.events = [
 		type: 'Concert',
 		eventUrl: 'https://www.facebook.com/events/1147403628795686/',
 		venueName: 'A-casă',
-		canceled: ''
-	},
-	{
-		act: 'Lucky Chops',
-		date: '2019-07-21',
-		type: 'Concert',
-		eventUrl: 'https://electriccastle.ro/schedule',
-		venueName: 'Bonțida',
-		canceled: ''
-	},
-	{
-		act: 'The Qualitons',
-		date: '2019-07-21',
-		type: 'Concert',
-		eventUrl: 'https://electriccastle.ro/schedule',
-		venueName: 'Bonțida',
-		canceled: ''
-	},
-	{
-		act: 'Handsome Furs',
-		date: '2019-07-21',
-		type: 'Live Set',
-		eventUrl: 'https://electriccastle.ro/schedule',
-		venueName: 'Bonțida',
-		canceled: ''
-	},
-	{
-		act: 'Metric',
-		date: '2019-07-21',
-		type: 'Concert',
-		eventUrl: 'https://electriccastle.ro/schedule',
-		venueName: 'Bonțida',
-		canceled: ''
-	},
-	{
-		act: 'Gato Preto',
-		date: '2019-07-21',
-		type: 'Concert',
-		eventUrl: 'https://electriccastle.ro/schedule',
-		venueName: 'Bonțida',
-		canceled: ''
-	},
-	{
-		act: 'Viagra Boys',
-		date: '2019-07-21',
-		type: 'Concert',
-		eventUrl: 'https://electriccastle.ro/schedule',
-		venueName: 'Bonțida',
-		canceled: ''
-	},
-	{
-		act: 'The Allergies',
-		date: '2019-07-21',
-		type: 'DJ Set',
-		eventUrl: 'https://electriccastle.ro/schedule',
-		venueName: 'Bonțida',
-		canceled: ''
-	},
-	{
-		act: 'The Slow Readers Club',
-		date: '2019-07-21',
-		type: 'Concert',
-		eventUrl: 'https://electriccastle.ro/schedule',
-		venueName: 'Bonțida',
-		canceled: ''
-	},
-	{
-		act: 'Greg Wilson',
-		date: '2019-07-21',
-		type: 'DJ Set',
-		eventUrl: 'https://electriccastle.ro/schedule',
-		venueName: 'Bonțida',
-		canceled: ''
-	},
-	{
-		act: 'EliEscobar',
-		date: '2019-07-21',
-		type: 'DJ Set',
-		eventUrl: 'https://electriccastle.ro/schedule',
-		venueName: 'Bonțida',
-		canceled: ''
-	},
-	{
-		act: 'The Herbaliser',
-		date: '2019-07-21',
-		type: 'DJ Set',
-		eventUrl: 'https://electriccastle.ro/schedule',
-		venueName: 'Bonțida',
 		canceled: ''
 	},
 	{
