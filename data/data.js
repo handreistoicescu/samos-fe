37862edfdf70bc3d5ab3d00774547db31cef3752
Drei <stoicescuofsuburbia@gmail.com>
2019-07-22 10:41:31 +0300

    Update events

--- data/data.js
+++ data/data.js
@@ -1,30 +1,5 @@
 exports.events = [
 	{
-		act:
-			'Samoș notice: due to the recent racist comments expressed by the organizers of the Electric Castle festival, we at Samos.ro have taken the decision of stopping the promotion of all events related to the aforementioned festival. We strongly encourage you to boycott the rest of the festival and redirect any funds you were intending to spend there to one of the various NGOs that support the Roma community.',
-		date: '2019-07-20',
-		type: 'Various concerts',
-		eventUrl: 'https://romanicriss.org/',
-		venueName: 'Bonțida',
-		canceled: ''
-	},
-	{
-		act: 'Louis C.K.',
-		date: '2019-07-21',
-		type: 'Stand-up Comedy',
-		eventUrl: 'https://www.facebook.com/events/196352844644962/',
-		venueName: 'Casa de Cultură a Studenților',
-		canceled: ''
-	},
-	{
-		act: 'Jah Cuzzi',
-		date: '2019-07-21',
-		type: 'Concert',
-		eventUrl: 'https://www.facebook.com/events/1147403628795686/',
-		venueName: 'A-casă',
-		canceled: ''
-	},
-	{
 		act: 'God Is An Astronaut',
 		date: '2019-07-23',
 		type: 'Concert',
