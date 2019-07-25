b98be0965730109dd7d826f6c3454028cbe46692
Drei <stoicescuofsuburbia@gmail.com>
2019-07-25 22:20:52 +0300

    Update events

--- data/data.js
+++ data/data.js
@@ -8,6 +8,14 @@ exports.events = [
 		canceled: ''
 	},
 	{
+		act: 'Lazzaro Felice',
+		date: '2019-07-26',
+		type: 'Movie',
+		eventUrl: 'https://www.facebook.com/events/2423597624591708/',
+		venueName: 'Cinema Victoria',
+		canceled: ''
+	},
+	{
 		act: 'Raisa Hagiu',
 		date: '2019-07-27',
 		type: 'Vernissage',
@@ -21,6 +29,230 @@ exports.events = [
 		type: 'DJ Set',
 		eventUrl: 'https://www.facebook.com/events/2335355129843395/',
 		venueName: 'Urania Palace',
+		canceled: ''
+	},
+	{
+		act: 'Andy Benz',
+		date: '2019-08-01',
+		type: 'Live Set',
+		eventUrl: 'https://untold.com',
+		venueName: 'Untold - Earth Spirits',
+		canceled: ''
+	},
+	{
+		act: 'Zimbru',
+		date: '2019-08-01',
+		type: 'Concert',
+		eventUrl: 'https://untold.com',
+		venueName: 'Untold - Forest',
+		canceled: ''
+	},
+	{
+		act: 'Oceanvs Orientalis',
+		date: '2019-08-01',
+		type: 'DJ Set',
+		eventUrl: 'https://untold.com',
+		venueName: 'Untold - Daydreaming',
+		canceled: ''
+	},
+	{
+		act: 'Audiofly',
+		date: '2019-08-01',
+		type: 'DJ Set',
+		eventUrl: 'https://untold.com',
+		venueName: 'Untold - Daydreaming',
+		canceled: ''
+	},
+	{
+		act: 'Alborosie & Shengen Clan',
+		date: '2019-08-01',
+		type: 'Concert',
+		eventUrl: 'https://untold.com',
+		venueName: 'Untold - Forest',
+		canceled: ''
+	},
+	{
+		act: 'Morcheeba',
+		date: '2019-08-01',
+		type: 'Concert',
+		eventUrl: 'https://untold.com',
+		venueName: 'Untold - Forest',
+		canceled: ''
+	},
+	{
+		act: 'Camo & Krooked',
+		date: '2019-08-01',
+		type: 'Live Set',
+		eventUrl: 'https://untold.com',
+		venueName: 'Untold - Alchemy',
+		canceled: ''
+	},
+	{
+		act: 'Loco Dice',
+		date: '2019-08-01',
+		type: 'DJ Set',
+		eventUrl: 'https://untold.com',
+		venueName: 'Untold - Galaxy',
+		canceled: ''
+	},
+	{
+		act: 'Monolink',
+		date: '2019-08-01',
+		type: 'Live Set',
+		eventUrl: 'https://untold.com',
+		venueName: 'Untold - Daydreaming',
+		canceled: ''
+	},
+	{
+		act: 'Nusha',
+		date: '2019-08-01',
+		type: 'DJ Set',
+		eventUrl: 'https://untold.com',
+		venueName: 'Untold - Time',
+		canceled: ''
+	},
+	{
+		act: 'Behrouz',
+		date: '2019-08-01',
+		type: 'DJ Set',
+		eventUrl: 'https://untold.com',
+		venueName: 'Untold - Daydreaming',
+		canceled: ''
+	},
+	{
+		act: 'Joseph Capriati',
+		date: '2019-08-01',
+		type: 'DJ Set',
+		eventUrl: 'https://untold.com',
+		venueName: 'Untold - Galaxy',
+		canceled: ''
+	},
+	{
+		act: 'Karpov not Kasparov',
+		date: '2019-08-02',
+		type: 'Concert',
+		eventUrl: 'https://untold.com',
+		venueName: 'Untold - Forest',
+		canceled: ''
+	},
+	{
+		act: 'Kerala Dust',
+		date: '2019-08-02',
+		type: 'DJ Set',
+		eventUrl: 'https://untold.com',
+		venueName: 'Untold - Daydreaming',
+		canceled: ''
+	},
+	{
+		act: 'Stormzy',
+		date: '2019-08-02',
+		type: 'Concert',
+		eventUrl: 'https://untold.com',
+		venueName: 'Untold - Main',
+		canceled: ''
+	},
+	{
+		act: 'BLOND:ISH',
+		date: '2019-08-02',
+		type: 'DJ Set',
+		eventUrl: 'https://untold.com',
+		venueName: 'Untold - Daydreaming',
+		canceled: ''
+	},
+	{
+		act: 'Busta Rhymes',
+		date: '2019-08-02',
+		type: 'Concert',
+		eventUrl: 'https://untold.com',
+		venueName: 'Untold - Main',
+		canceled: ''
+	},
+	{
+		act: 'Komraus',
+		date: '2019-08-02',
+		type: 'Concert',
+		eventUrl: 'https://untold.com',
+		venueName: 'Untold - Forest',
+		canceled: ''
+	},
+	{
+		act: 'Acid Pauli',
+		date: '2019-08-02',
+		type: 'DJ Set',
+		eventUrl: 'https://untold.com',
+		venueName: 'Untold - Daydreaming',
+		canceled: ''
+	},
+	{
+		act: 'Akua Naru',
+		date: '2019-08-02',
+		type: 'Concert',
+		eventUrl: 'https://untold.com',
+		venueName: 'Untold - Forest',
+		canceled: ''
+	},
+	{
+		act: 'Solomun',
+		date: '2019-08-02',
+		type: 'DJ Set',
+		eventUrl: 'https://untold.com',
+		venueName: 'Untold - Galaxy',
+		canceled: ''
+	},
+	{
+		act: 'Dave Seaman',
+		date: '2019-08-02',
+		type: 'DJ Set',
+		eventUrl: 'https://untold.com',
+		venueName: 'Untold - Fortune',
+		canceled: ''
+	},
+	{
+		act: 'Scarlett Etienne',
+		date: '2019-08-02',
+		type: 'DJ Set',
+		eventUrl: 'https://untold.com',
+		venueName: 'Untold - Time',
+		canceled: ''
+	},
+	{
+		act: 'Damian Lazarus',
+		date: '2019-08-02',
+		type: 'DJ Set',
+		eventUrl: 'https://untold.com',
+		venueName: 'Untold - Daydreaming',
+		canceled: ''
+	},
+	{
+		act: 'Sébastien Léger',
+		date: '2019-08-02',
+		type: 'DJ Set',
+		eventUrl: 'https://untold.com',
+		venueName: 'Untold - Fortune',
+		canceled: ''
+	},
+	{
+		act: 'Chase & Status',
+		date: '2019-08-02',
+		type: 'DJ Set',
+		eventUrl: 'https://untold.com',
+		venueName: 'Untold - Alchemy',
+		canceled: ''
+	},
+	{
+		act: 'Goncalo',
+		date: '2019-08-02',
+		type: 'DJ Set',
+		eventUrl: 'https://untold.com',
+		venueName: 'Untold - Time',
+		canceled: ''
+	},
+	{
+		act: 'Boris Brejcha',
+		date: '2019-08-02',
+		type: 'DJ Set',
+		eventUrl: 'https://untold.com',
+		venueName: 'Untold - Galaxy',
 		canceled: ''
 	}
 ];
