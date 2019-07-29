42573c37f9e5fa3053ba78f2c8f061aa1521b9d9
Drei <stoicescuofsuburbia@gmail.com>
2019-07-29 08:34:40 +0300

    Update events

--- data/data.js
+++ data/data.js
@@ -1,21 +1,5 @@
 exports.events = [
 	{
-		act: 'Raisa Hagiu',
-		date: '2019-07-27',
-		type: 'Vernissage',
-		eventUrl: 'https://www.facebook.com/events/348188149415257/',
-		venueName: 'White Cuib',
-		canceled: ''
-	},
-	{
-		act: 'Saboar',
-		date: '2019-07-27',
-		type: 'DJ Set',
-		eventUrl: 'https://www.facebook.com/events/2335355129843395/',
-		venueName: 'Urania Palace',
-		canceled: ''
-	},
-	{
 		act: 'Andy Benz',
 		date: '2019-08-01',
 		type: 'Live Set',
