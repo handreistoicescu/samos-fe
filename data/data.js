d0d63f0c7248d534924947cf51ade85e6d6504ae
Drei <stoicescuofsuburbia@gmail.com>
2019-08-02 09:49:34 +0300

    Update events

--- data/data.js
+++ data/data.js
@@ -1,442 +1,330 @@
 exports.events = [
-  {
-    "act": "Stat, Poliție, Violență",
-    "date": "2019-08-01",
-    "type": "Discussion",
-    "eventUrl": "https://www.facebook.com/events/2010254429056158",
-    "venueName": "A-casă",
-    "canceled": ""
-  },
-  {
-    "act": "Banksy",
-    "date": "2019-08-01",
-    "type": "Vernissage",
-    "eventUrl": "https://expozitiatheartofbanksy.ro/cluj/",
-    "venueName": "Casa Vaida Voevod",
-    "canceled": ""
-  },
-  {
-    "act": "Zimbru",
-    "date": "2019-08-01",
-    "type": "Concert",
-    "eventUrl": "https://untold.com",
-    "venueName": "Untold - Forest",
-    "canceled": ""
-  },
-  {
-    "act": "Oceanvs Orientalis",
-    "date": "2019-08-01",
-    "type": "DJ Set",
-    "eventUrl": "https://untold.com",
-    "venueName": "Untold - Daydreaming",
-    "canceled": ""
-  },
-  {
-    "act": "Audiofly",
-    "date": "2019-08-01",
-    "type": "DJ Set",
-    "eventUrl": "https://untold.com",
-    "venueName": "Untold - Daydreaming",
-    "canceled": ""
-  },
-  {
-    "act": "Alborosie & Shengen Clan",
-    "date": "2019-08-01",
-    "type": "Concert",
-    "eventUrl": "https://untold.com",
-    "venueName": "Untold - Forest",
-    "canceled": ""
-  },
-  {
-    "act": "Morcheeba",
-    "date": "2019-08-01",
-    "type": "Concert",
-    "eventUrl": "https://untold.com",
-    "venueName": "Untold - Forest",
-    "canceled": ""
-  },
-  {
-    "act": "Camo & Krooked",
-    "date": "2019-08-01",
-    "type": "Live Set",
-    "eventUrl": "https://untold.com",
-    "venueName": "Untold - Alchemy",
-    "canceled": ""
-  },
-  {
-    "act": "Loco Dice",
-    "date": "2019-08-01",
-    "type": "DJ Set",
-    "eventUrl": "https://untold.com",
-    "venueName": "Untold - Galaxy",
-    "canceled": ""
-  },
-  {
-    "act": "Monolink",
-    "date": "2019-08-01",
-    "type": "Live Set",
-    "eventUrl": "https://untold.com",
-    "venueName": "Untold - Daydreaming",
-    "canceled": ""
-  },
-  {
-    "act": "Nusha",
-    "date": "2019-08-01",
-    "type": "DJ Set",
-    "eventUrl": "https://untold.com",
-    "venueName": "Untold - Time",
-    "canceled": ""
-  },
-  {
-    "act": "Behrouz",
-    "date": "2019-08-01",
-    "type": "DJ Set",
-    "eventUrl": "https://untold.com",
-    "venueName": "Untold - Daydreaming",
-    "canceled": ""
-  },
-  {
-    "act": "Joseph Capriati",
-    "date": "2019-08-01",
-    "type": "DJ Set",
-    "eventUrl": "https://untold.com",
-    "venueName": "Untold - Galaxy",
-    "canceled": ""
-  },
-  {
-    "act": "Kjartan & Hét Hat Club",
-    "date": "2019-08-01",
-    "type": "Concert",
-    "eventUrl": "http://www.meraworldmusic.com/en/program",
-    "venueName": "Mera",
-    "canceled": ""
-  },
-  {
-    "act": "Karpov not Kasparov",
-    "date": "2019-08-02",
-    "type": "Concert",
-    "eventUrl": "https://untold.com",
-    "venueName": "Untold - Forest",
-    "canceled": ""
-  },
-  {
-    "act": "Kerala Dust",
-    "date": "2019-08-02",
-    "type": "DJ Set",
-    "eventUrl": "https://untold.com",
-    "venueName": "Untold - Daydreaming",
-    "canceled": ""
-  },
-  {
-    "act": "Stormzy",
-    "date": "2019-08-02",
-    "type": "Concert",
-    "eventUrl": "https://untold.com",
-    "venueName": "Untold - Main",
-    "canceled": ""
-  },
-  {
-    "act": "BLOND:ISH",
-    "date": "2019-08-02",
-    "type": "DJ Set",
-    "eventUrl": "https://untold.com",
-    "venueName": "Untold - Daydreaming",
-    "canceled": ""
-  },
-  {
-    "act": "Busta Rhymes",
-    "date": "2019-08-02",
-    "type": "Concert",
-    "eventUrl": "https://untold.com",
-    "venueName": "Untold - Main",
-    "canceled": ""
-  },
-  {
-    "act": "Komraus",
-    "date": "2019-08-02",
-    "type": "Concert",
-    "eventUrl": "https://untold.com",
-    "venueName": "Untold - Forest",
-    "canceled": ""
-  },
-  {
-    "act": "Acid Pauli",
-    "date": "2019-08-02",
-    "type": "DJ Set",
-    "eventUrl": "https://untold.com",
-    "venueName": "Untold - Daydreaming",
-    "canceled": ""
-  },
-  {
-    "act": "Akua Naru",
-    "date": "2019-08-02",
-    "type": "Concert",
-    "eventUrl": "https://untold.com",
-    "venueName": "Untold - Forest",
-    "canceled": ""
-  },
-  {
-    "act": "Solomun",
-    "date": "2019-08-02",
-    "type": "DJ Set",
-    "eventUrl": "https://untold.com",
-    "venueName": "Untold - Galaxy",
-    "canceled": ""
-  },
-  {
-    "act": "Dave Seaman",
-    "date": "2019-08-02",
-    "type": "DJ Set",
-    "eventUrl": "https://untold.com",
-    "venueName": "Untold - Fortune",
-    "canceled": ""
-  },
-  {
-    "act": "Scarlett Etienne",
-    "date": "2019-08-02",
-    "type": "DJ Set",
-    "eventUrl": "https://untold.com",
-    "venueName": "Untold - Time",
-    "canceled": ""
-  },
-  {
-    "act": "Damian Lazarus",
-    "date": "2019-08-02",
-    "type": "DJ Set",
-    "eventUrl": "https://untold.com",
-    "venueName": "Untold - Daydreaming",
-    "canceled": ""
-  },
-  {
-    "act": "Sébastien Léger",
-    "date": "2019-08-02",
-    "type": "DJ Set",
-    "eventUrl": "https://untold.com",
-    "venueName": "Untold - Fortune",
-    "canceled": ""
-  },
-  {
-    "act": "Chase & Status",
-    "date": "2019-08-02",
-    "type": "DJ Set",
-    "eventUrl": "https://untold.com",
-    "venueName": "Untold - Alchemy",
-    "canceled": ""
-  },
-  {
-    "act": "Goncalo",
-    "date": "2019-08-02",
-    "type": "DJ Set",
-    "eventUrl": "https://untold.com",
-    "venueName": "Untold - Time",
-    "canceled": ""
-  },
-  {
-    "act": "Boris Brejcha",
-    "date": "2019-08-02",
-    "type": "DJ Set",
-    "eventUrl": "https://untold.com",
-    "venueName": "Untold - Galaxy",
-    "canceled": ""
-  },
-  {
-    "act": "Trei parale",
-    "date": "2019-08-02",
-    "type": "Concert",
-    "eventUrl": "http://www.meraworldmusic.com/en/program",
-    "venueName": "Mera",
-    "canceled": ""
-  },
-  {
-    "act": "Black Motion",
-    "date": "2019-08-03",
-    "type": "Live Set",
-    "eventUrl": "https://untold.com",
-    "venueName": "Untold - Daydreaming",
-    "canceled": ""
-  },
-  {
-    "act": "Bedouin",
-    "date": "2019-08-03",
-    "type": "DJ Set",
-    "eventUrl": "https://untold.com",
-    "venueName": "Untold - Daydreaming",
-    "canceled": ""
-  },
-  {
-    "act": "Paul Kalkbrenner",
-    "date": "2019-08-03",
-    "type": "DJ Set",
-    "eventUrl": "https://untold.com",
-    "venueName": "Untold - Galaxy",
-    "canceled": ""
-  },
-  {
-    "act": "Mihai Popoviciu",
-    "date": "2019-08-03",
-    "type": "DJ Set",
-    "eventUrl": "https://untold.com",
-    "venueName": "Untold - Time",
-    "canceled": ""
-  },
-  {
-    "act": "Sevdaliza",
-    "date": "2019-08-03",
-    "type": "Concert",
-    "eventUrl": "https://untold.com",
-    "venueName": "Untold - Forest",
-    "canceled": ""
-  },
-  {
-    "act": "Tale Of Us",
-    "date": "2019-08-03",
-    "type": "DJ Set",
-    "eventUrl": "https://untold.com",
-    "venueName": "Untold - Galaxy",
-    "canceled": ""
-  },
-  {
-    "act": "Sub Focus",
-    "date": "2019-08-03",
-    "type": "DJ Set",
-    "eventUrl": "https://untold.com",
-    "venueName": "Untold - Alchemy",
-    "canceled": ""
-  },
-  {
-    "act": "Guy Gerber",
-    "date": "2019-08-03",
-    "type": "DJ Set",
-    "eventUrl": "https://untold.com",
-    "venueName": "Untold - Daydreaming",
-    "canceled": ""
-  },
-  {
-    "act": "Near East Collective",
-    "date": "2019-08-03",
-    "type": "Concert",
-    "eventUrl": "http://www.meraworldmusic.com/en/program",
-    "venueName": "Mera",
-    "canceled": ""
-  },
-  {
-    "act": "Tinie Tempah",
-    "date": "2019-08-04",
-    "type": "DJ Set",
-    "eventUrl": "https://untold.com",
-    "venueName": "Untold - Main",
-    "canceled": ""
-  },
-  {
-    "act": "Black Coffee",
-    "date": "2019-08-04",
-    "type": "DJ Set",
-    "eventUrl": "https://untold.com",
-    "venueName": "Untold - Daydreaming",
-    "canceled": ""
-  },
-  {
-    "act": "Inner Circle",
-    "date": "2019-08-04",
-    "type": "Concert",
-    "eventUrl": "https://untold.com",
-    "venueName": "Untold - Forest",
-    "canceled": ""
-  },
-  {
-    "act": "Robbie Williams",
-    "date": "2019-08-04",
-    "type": "Concert",
-    "eventUrl": "https://untold.com",
-    "venueName": "Untold - Main",
-    "canceled": ""
-  },
-  {
-    "act": "Nora En Pure",
-    "date": "2019-08-04",
-    "type": "DJ Set",
-    "eventUrl": "https://untold.com",
-    "venueName": "Untold - Time",
-    "canceled": ""
-  },
-  {
-    "act": "Krafty Kuts",
-    "date": "2019-08-04",
-    "type": "DJ Set",
-    "eventUrl": "https://untold.com",
-    "venueName": "Untold - Alchemy",
-    "canceled": ""
-  },
-  {
-    "act": "Seth Troxler",
-    "date": "2019-08-04",
-    "type": "DJ Set",
-    "eventUrl": "https://untold.com",
-    "venueName": "Untold - Galaxy",
-    "canceled": ""
-  },
-  {
-    "act": "Tanya Stephens",
-    "date": "2019-08-04",
-    "type": "Concert",
-    "eventUrl": "https://untold.com",
-    "venueName": "Untold - Forest",
-    "canceled": ""
-  },
-  {
-    "act": "Dub FX",
-    "date": "2019-08-04",
-    "type": "Live Set",
-    "eventUrl": "https://untold.com",
-    "venueName": "Untold - Alchemy",
-    "canceled": ""
-  },
-  {
-    "act": "Lazare Hoche",
-    "date": "2019-08-04",
-    "type": "DJ Set",
-    "eventUrl": "https://untold.com",
-    "venueName": "Untold - Daydreaming",
-    "canceled": ""
-  },
-  {
-    "act": "Jamie Jones",
-    "date": "2019-08-04",
-    "type": "DJ Set",
-    "eventUrl": "https://untold.com",
-    "venueName": "Untold - Galaxy",
-    "canceled": ""
-  },
-  {
-    "act": "Vassvik",
-    "date": "2019-08-04",
-    "type": "Concert",
-    "eventUrl": "http://www.meraworldmusic.com/en/program",
-    "venueName": "Mera",
-    "canceled": ""
-  },
-  {
-    "act": "Dikanda",
-    "date": "2019-08-04",
-    "type": "Concert",
-    "eventUrl": "http://www.meraworldmusic.com/en/program",
-    "venueName": "Mera",
-    "canceled": ""
-  },
-  {
-    "act": "The Secret Garden",
-    "date": "2019-08-08",
-    "type": "Zine Launch",
-    "eventUrl": "https://www.facebook.com/events/2373006626281168/",
-    "venueName": "Colectiva Gazette",
-    "canceled": ""
-  },
-  {
-    "act": "The Man Who Killed Don Quixote",
-    "date": "2019-08-09",
-    "type": "Movie",
-    "eventUrl": "https://www.facebook.com/events/308481200087919",
-    "venueName": "Iulius Parc",
-    "canceled": ""
-  }
-]
+	{
+		act: 'Karpov not Kasparov',
+		date: '2019-08-02',
+		type: 'Concert',
+		eventUrl: 'https://untold.com',
+		venueName: 'Untold - Forest',
+		canceled: ''
+	},
+	{
+		act: 'Kerala Dust',
+		date: '2019-08-02',
+		type: 'DJ Set',
+		eventUrl: 'https://untold.com',
+		venueName: 'Untold - Daydreaming',
+		canceled: ''
+	},
+	{
+		act: 'Stormzy',
+		date: '2019-08-02',
+		type: 'Concert',
+		eventUrl: 'https://untold.com',
+		venueName: 'Untold - Main',
+		canceled: ''
+	},
+	{
+		act: 'BLOND:ISH',
+		date: '2019-08-02',
+		type: 'DJ Set',
+		eventUrl: 'https://untold.com',
+		venueName: 'Untold - Daydreaming',
+		canceled: ''
+	},
+	{
+		act: 'Busta Rhymes',
+		date: '2019-08-02',
+		type: 'Concert',
+		eventUrl: 'https://untold.com',
+		venueName: 'Untold - Main',
+		canceled: ''
+	},
+	{
+		act: 'Komraus',
+		date: '2019-08-02',
+		type: 'Concert',
+		eventUrl: 'https://untold.com',
+		venueName: 'Untold - Forest',
+		canceled: ''
+	},
+	{
+		act: 'Acid Pauli',
+		date: '2019-08-02',
+		type: 'DJ Set',
+		eventUrl: 'https://untold.com',
+		venueName: 'Untold - Daydreaming',
+		canceled: ''
+	},
+	{
+		act: 'Akua Naru',
+		date: '2019-08-02',
+		type: 'Concert',
+		eventUrl: 'https://untold.com',
+		venueName: 'Untold - Forest',
+		canceled: ''
+	},
+	{
+		act: 'Solomun',
+		date: '2019-08-02',
+		type: 'DJ Set',
+		eventUrl: 'https://untold.com',
+		venueName: 'Untold - Galaxy',
+		canceled: ''
+	},
+	{
+		act: 'Dave Seaman',
+		date: '2019-08-02',
+		type: 'DJ Set',
+		eventUrl: 'https://untold.com',
+		venueName: 'Untold - Fortune',
+		canceled: ''
+	},
+	{
+		act: 'Scarlett Etienne',
+		date: '2019-08-02',
+		type: 'DJ Set',
+		eventUrl: 'https://untold.com',
+		venueName: 'Untold - Time',
+		canceled: ''
+	},
+	{
+		act: 'Damian Lazarus',
+		date: '2019-08-02',
+		type: 'DJ Set',
+		eventUrl: 'https://untold.com',
+		venueName: 'Untold - Daydreaming',
+		canceled: ''
+	},
+	{
+		act: 'Sébastien Léger',
+		date: '2019-08-02',
+		type: 'DJ Set',
+		eventUrl: 'https://untold.com',
+		venueName: 'Untold - Fortune',
+		canceled: ''
+	},
+	{
+		act: 'Chase & Status',
+		date: '2019-08-02',
+		type: 'DJ Set',
+		eventUrl: 'https://untold.com',
+		venueName: 'Untold - Alchemy',
+		canceled: ''
+	},
+	{
+		act: 'Goncalo',
+		date: '2019-08-02',
+		type: 'DJ Set',
+		eventUrl: 'https://untold.com',
+		venueName: 'Untold - Time',
+		canceled: ''
+	},
+	{
+		act: 'Boris Brejcha',
+		date: '2019-08-02',
+		type: 'DJ Set',
+		eventUrl: 'https://untold.com',
+		venueName: 'Untold - Galaxy',
+		canceled: ''
+	},
+	{
+		act: 'Trei parale',
+		date: '2019-08-02',
+		type: 'Concert',
+		eventUrl: 'http://www.meraworldmusic.com/en/program',
+		venueName: 'Mera',
+		canceled: ''
+	},
+	{
+		act: 'Black Motion',
+		date: '2019-08-03',
+		type: 'Live Set',
+		eventUrl: 'https://untold.com',
+		venueName: 'Untold - Daydreaming',
+		canceled: ''
+	},
+	{
+		act: 'Bedouin',
+		date: '2019-08-03',
+		type: 'DJ Set',
+		eventUrl: 'https://untold.com',
+		venueName: 'Untold - Daydreaming',
+		canceled: ''
+	},
+	{
+		act: 'Paul Kalkbrenner',
+		date: '2019-08-03',
+		type: 'DJ Set',
+		eventUrl: 'https://untold.com',
+		venueName: 'Untold - Galaxy',
+		canceled: ''
+	},
+	{
+		act: 'Mihai Popoviciu',
+		date: '2019-08-03',
+		type: 'DJ Set',
+		eventUrl: 'https://untold.com',
+		venueName: 'Untold - Time',
+		canceled: ''
+	},
+	{
+		act: 'Sevdaliza',
+		date: '2019-08-03',
+		type: 'Concert',
+		eventUrl: 'https://untold.com',
+		venueName: 'Untold - Forest',
+		canceled: ''
+	},
+	{
+		act: 'Tale Of Us',
+		date: '2019-08-03',
+		type: 'DJ Set',
+		eventUrl: 'https://untold.com',
+		venueName: 'Untold - Galaxy',
+		canceled: ''
+	},
+	{
+		act: 'Sub Focus',
+		date: '2019-08-03',
+		type: 'DJ Set',
+		eventUrl: 'https://untold.com',
+		venueName: 'Untold - Alchemy',
+		canceled: ''
+	},
+	{
+		act: 'Guy Gerber',
+		date: '2019-08-03',
+		type: 'DJ Set',
+		eventUrl: 'https://untold.com',
+		venueName: 'Untold - Daydreaming',
+		canceled: ''
+	},
+	{
+		act: 'Near East Collective',
+		date: '2019-08-03',
+		type: 'Concert',
+		eventUrl: 'http://www.meraworldmusic.com/en/program',
+		venueName: 'Mera',
+		canceled: ''
+	},
+	{
+		act: 'Tinie Tempah',
+		date: '2019-08-04',
+		type: 'DJ Set',
+		eventUrl: 'https://untold.com',
+		venueName: 'Untold - Main',
+		canceled: ''
+	},
+	{
+		act: 'Black Coffee',
+		date: '2019-08-04',
+		type: 'DJ Set',
+		eventUrl: 'https://untold.com',
+		venueName: 'Untold - Daydreaming',
+		canceled: ''
+	},
+	{
+		act: 'Inner Circle',
+		date: '2019-08-04',
+		type: 'Concert',
+		eventUrl: 'https://untold.com',
+		venueName: 'Untold - Forest',
+		canceled: ''
+	},
+	{
+		act: 'Robbie Williams',
+		date: '2019-08-04',
+		type: 'Concert',
+		eventUrl: 'https://untold.com',
+		venueName: 'Untold - Main',
+		canceled: ''
+	},
+	{
+		act: 'Nora En Pure',
+		date: '2019-08-04',
+		type: 'DJ Set',
+		eventUrl: 'https://untold.com',
+		venueName: 'Untold - Time',
+		canceled: ''
+	},
+	{
+		act: 'Krafty Kuts',
+		date: '2019-08-04',
+		type: 'DJ Set',
+		eventUrl: 'https://untold.com',
+		venueName: 'Untold - Alchemy',
+		canceled: ''
+	},
+	{
+		act: 'Seth Troxler',
+		date: '2019-08-04',
+		type: 'DJ Set',
+		eventUrl: 'https://untold.com',
+		venueName: 'Untold - Galaxy',
+		canceled: ''
+	},
+	{
+		act: 'Tanya Stephens',
+		date: '2019-08-04',
+		type: 'Concert',
+		eventUrl: 'https://untold.com',
+		venueName: 'Untold - Forest',
+		canceled: ''
+	},
+	{
+		act: 'Dub FX',
+		date: '2019-08-04',
+		type: 'Live Set',
+		eventUrl: 'https://untold.com',
+		venueName: 'Untold - Alchemy',
+		canceled: ''
+	},
+	{
+		act: 'Lazare Hoche',
+		date: '2019-08-04',
+		type: 'DJ Set',
+		eventUrl: 'https://untold.com',
+		venueName: 'Untold - Daydreaming',
+		canceled: ''
+	},
+	{
+		act: 'Jamie Jones',
+		date: '2019-08-04',
+		type: 'DJ Set',
+		eventUrl: 'https://untold.com',
+		venueName: 'Untold - Galaxy',
+		canceled: ''
+	},
+	{
+		act: 'Vassvik',
+		date: '2019-08-04',
+		type: 'Concert',
+		eventUrl: 'http://www.meraworldmusic.com/en/program',
+		venueName: 'Mera',
+		canceled: ''
+	},
+	{
+		act: 'Dikanda',
+		date: '2019-08-04',
+		type: 'Concert',
+		eventUrl: 'http://www.meraworldmusic.com/en/program',
+		venueName: 'Mera',
+		canceled: ''
+	},
+	{
+		act: 'The Secret Garden',
+		date: '2019-08-08',
+		type: 'Zine Launch',
+		eventUrl: 'https://www.facebook.com/events/2373006626281168/',
+		venueName: 'Colectiva Gazette',
+		canceled: ''
+	},
+	{
+		act: 'The Man Who Killed Don Quixote',
+		date: '2019-08-09',
+		type: 'Movie',
+		eventUrl: 'https://www.facebook.com/events/308481200087919',
+		venueName: 'Iulius Parc',
+		canceled: ''
+	}
+];
