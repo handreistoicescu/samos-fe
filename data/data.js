4b3e5f51b680a0463632dd507d5fdf2ccca9ba79
Drei <stoicescuofsuburbia@gmail.com>
2019-08-07 17:10:58 +0300

    Update events

--- data/data.js
+++ data/data.js
@@ -1,9 +1,33 @@
 exports.events = [
 	{
+		act: 'Climate Justice Action',
+		date: '2019-08-08',
+		type: 'Discussion',
+		eventUrl: 'https://www.facebook.com/events/2010254429056158',
+		venueName: 'A-casă',
+		canceled: ''
+	},
+	{
+		act: 'Radu Valcu Quartet',
+		date: '2019-08-08',
+		type: 'Concert',
+		eventUrl: 'https://www.facebook.com/events/1232377296935707/',
+		venueName: 'Sisters',
+		canceled: ''
+	},
+	{
 		act: 'The Secret Garden',
 		date: '2019-08-08',
 		type: 'Zine Launch',
 		eventUrl: 'https://www.facebook.com/events/2373006626281168/',
+		venueName: 'Colectiva Gazette',
+		canceled: ''
+	},
+	{
+		act: 'Manifesto',
+		date: '2019-08-09',
+		type: 'Movie',
+		eventUrl: 'https://www.facebook.com/events/517541075453598/',
 		venueName: 'Colectiva Gazette',
 		canceled: ''
 	},
@@ -13,6 +37,46 @@ exports.events = [
 		type: 'Movie',
 		eventUrl: 'https://www.facebook.com/events/308481200087919',
 		venueName: 'Iulius Parc',
+		canceled: ''
+	},
+	{
+		act: 'Nik Bärtsch’s Ronin',
+		date: '2019-08-15',
+		type: 'Concert',
+		eventUrl: 'www.smidajazz.ro',
+		venueName: 'Smida',
+		canceled: ''
+	},
+	{
+		act: 'Mihaela Hudrea',
+		date: '2019-08-16',
+		type: 'Vernissage',
+		eventUrl: '',
+		venueName: 'White Cuib',
+		canceled: ''
+	},
+	{
+		act: 'James Holden & The Animal Spirits',
+		date: '2019-08-16',
+		type: 'Concert',
+		eventUrl: 'www.smidajazz.ro',
+		venueName: 'Smida',
+		canceled: ''
+	},
+	{
+		act: 'Mammal Hands',
+		date: '2019-08-17',
+		type: 'Concert',
+		eventUrl: 'www.smidajazz.ro',
+		venueName: 'Smida',
+		canceled: ''
+	},
+	{
+		act: 'Rómeó és Júlia',
+		date: '2019-08-19',
+		type: 'Musical',
+		eventUrl: 'https://www.facebook.com/events/484795245600771/',
+		venueName: 'Opera Maghiară Cluj',
 		canceled: ''
 	}
 ];
