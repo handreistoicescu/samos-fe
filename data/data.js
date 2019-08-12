c71f4c076a261eccb5fbe27d0cc475127c8094a0
Drei <stoicescuofsuburbia@gmail.com>
2019-08-12 13:10:43 +0300

    Update events

--- data/data.js
+++ data/data.js
@@ -1,5 +1,13 @@
 exports.events = [
 	{
+		act: 'Gy. Szabó Béla',
+		date: '2019-08-14',
+		type: 'Vernissage',
+		eventUrl: 'https://www.facebook.com/events/2906648286072947/',
+		venueName: 'Galeria Quadro',
+		canceled: ''
+	},
+	{
 		act: 'Nik Bärtsch’s Ronin',
 		date: '2019-08-15',
 		type: 'Concert',
@@ -24,6 +32,14 @@ exports.events = [
 		canceled: ''
 	},
 	{
+		act: 'Once Upon a Time in Hollywood',
+		date: '2019-08-16',
+		type: 'Movie',
+		eventUrl: 'https://www.facebook.com/events/504492703427259/',
+		venueName: 'Cinema Victoria',
+		canceled: ''
+	},
+	{
 		act: 'Mammal Hands',
 		date: '2019-08-17',
 		type: 'Concert',
@@ -37,6 +53,14 @@ exports.events = [
 		type: 'Musical',
 		eventUrl: 'https://www.facebook.com/events/484795245600771/',
 		venueName: 'Opera Maghiară Cluj',
+		canceled: ''
+	},
+	{
+		act: 'Barac',
+		date: '2019-08-24',
+		type: 'DJ Set',
+		eventUrl: 'https://www.facebook.com/events/356178915273758/',
+		venueName: 'CREIC',
 		canceled: ''
 	}
 ];
