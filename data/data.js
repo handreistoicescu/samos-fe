19d7c70032e54d56cc500adba6feda0d18d941e9
Drei <stoicescuofsuburbia@gmail.com>
2019-08-17 22:43:24 +0300

    Update events

--- data/data.js
+++ data/data.js
@@ -1,53 +1,5 @@
 exports.events = [
 	{
-		act: 'Romhány Veronika',
-		date: '2019-08-15',
-		type: 'Vernissage',
-		eventUrl: 'https://www.facebook.com/events/462030647684932/',
-		venueName: 'Lateral ArtSpace',
-		canceled: ''
-	},
-	{
-		act: 'Nik Bärtsch’s Ronin',
-		date: '2019-08-15',
-		type: 'Concert',
-		eventUrl: 'www.smidajazz.ro',
-		venueName: 'Smida',
-		canceled: ''
-	},
-	{
-		act: 'Mihaela Hudrea',
-		date: '2019-08-16',
-		type: 'Vernissage',
-		eventUrl: 'https://www.facebook.com/events/413677912602128/',
-		venueName: 'White Cuib',
-		canceled: ''
-	},
-	{
-		act: 'Márton Zoltán',
-		date: '2019-08-16',
-		type: 'Vernissage',
-		eventUrl: 'https://www.facebook.com/events/812275262523452/',
-		venueName: 'BAZIS Project Space',
-		canceled: ''
-	},
-	{
-		act: 'James Holden & The Animal Spirits',
-		date: '2019-08-16',
-		type: 'Concert',
-		eventUrl: 'www.smidajazz.ro',
-		venueName: 'Smida',
-		canceled: ''
-	},
-	{
-		act: 'Once Upon a Time in Hollywood',
-		date: '2019-08-16',
-		type: 'Movie',
-		eventUrl: 'https://www.facebook.com/events/504492703427259/',
-		venueName: 'Cinema Victoria',
-		canceled: ''
-	},
-	{
 		act: 'Lea Rasovzsky și Flaviu Cacoveanu',
 		date: '2019-08-17',
 		type: 'Vernissage',
