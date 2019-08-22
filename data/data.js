e23c1153eadaceefc3b2984dddf4880191a4c66b
Drei <stoicescuofsuburbia@gmail.com>
2019-08-22 10:03:28 +0300

    Update events

--- data/data.js
+++ data/data.js
@@ -1,34 +1,26 @@
 exports.events = [
 	{
-		act: 'Kiméra',
-		date: '2019-08-19',
-		type: 'Concert',
-		eventUrl: 'https://www.facebook.com/events/407337869891253/',
-		venueName: 'Casa Tranzit',
-		canceled: ''
-	},
-	{
-		act: 'Rómeó és Júlia',
-		date: '2019-08-19',
-		type: 'Musical',
-		eventUrl: 'https://www.facebook.com/events/484795245600771/',
-		venueName: 'Opera Maghiară Cluj',
-		canceled: ''
-	},
-	{
-		act: 'Being Elsewhere',
-		date: '2019-08-21',
-		type: 'Performance Art',
-		eventUrl: 'https://www.facebook.com/events/482069085701598/',
-		venueName: 'Casa Tranzit',
-		canceled: ''
-	},
-	{
 		act: 'Barac',
 		date: '2019-08-24',
 		type: 'DJ Set',
 		eventUrl: 'https://www.facebook.com/events/356178915273758/',
 		venueName: 'CREIC',
+		canceled: ''
+	},
+	{
+		act: 'Lizz',
+		date: '2019-08-30',
+		type: 'DJ Set',
+		eventUrl: 'https://www.facebook.com/events/363349191228199/',
+		venueName: 'Cluj Arena',
+		canceled: ''
+	},
+	{
+		act: 'Morild',
+		date: '2019-09-03',
+		type: 'Concert',
+		eventUrl: 'https://www.facebook.com/events/467047377195122/',
+		venueName: 'Urania Palace',
 		canceled: ''
 	}
 ];
