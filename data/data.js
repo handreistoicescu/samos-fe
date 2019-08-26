2e7fbcf71e012ce365ef72fab28aacf899f79b6c
Drei <stoicescuofsuburbia@gmail.com>
2019-08-26 19:49:18 +0300

    Update events

--- data/data.js
+++ data/data.js
@@ -1,5 +1,13 @@
 exports.events = [
 	{
+		act: 'Serabulho',
+		date: '2019-08-27',
+		type: 'Concert',
+		eventUrl: 'https://www.facebook.com/events/303454587197268/',
+		venueName: 'Flying Circus',
+		canceled: ''
+	},
+	{
 		act: 'Lizz',
 		date: '2019-08-30',
 		type: 'DJ Set',
@@ -8,10 +16,66 @@ exports.events = [
 		canceled: ''
 	},
 	{
+		act: 'Wings of Desire (1987)',
+		date: '2019-08-31',
+		type: 'Movie',
+		eventUrl: 'https://www.facebook.com/events/413598925953312/',
+		venueName: 'Colectiva Gazette',
+		canceled: ''
+	},
+	{
 		act: 'Morild',
 		date: '2019-09-03',
 		type: 'Concert',
 		eventUrl: 'https://www.facebook.com/events/467047377195122/',
+		venueName: 'Urania Palace',
+		canceled: ''
+	},
+	{
+		act: 'Dans contemporan',
+		date: '2019-09-07',
+		type: 'Workshop',
+		eventUrl: 'https://www.facebook.com/events/477585419732259/',
+		venueName: 'Fabrica de Pensule',
+		canceled: ''
+	},
+	{
+		act: 'Primărie',
+		date: '2019-09-07',
+		type: 'DJ Set',
+		eventUrl: 'https://www.facebook.com/events/517000599069357/',
+		venueName: 'Gazette',
+		canceled: ''
+	},
+	{
+		act: 'Downfall of Gaia',
+		date: '2019-09-13',
+		type: 'Concert',
+		eventUrl: 'https://www.facebook.com/events/380775452415400/',
+		venueName: 'Form Space',
+		canceled: ''
+	},
+	{
+		act: 'Dans contemporan',
+		date: '2019-09-14',
+		type: 'Workshop',
+		eventUrl: 'https://www.facebook.com/events/477585419732259/',
+		venueName: 'Fabrica de Pensule',
+		canceled: ''
+	},
+	{
+		act: 'Shokran',
+		date: '2019-09-15',
+		type: 'Concert',
+		eventUrl: 'https://www.facebook.com/events/1369811869843692/',
+		venueName: 'Flying Circus',
+		canceled: ''
+	},
+	{
+		act: 'Kentawr',
+		date: '2019-09-16',
+		type: 'Concert',
+		eventUrl: 'https://www.facebook.com/events/879865329051520/',
 		venueName: 'Urania Palace',
 		canceled: ''
 	}
