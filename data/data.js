96bed0d4890e901e55bd1f6dcbbf219dac296fd3
Drei <stoicescuofsuburbia@gmail.com>
2019-08-30 15:33:21 +0300

    Update events

--- data/data.js
+++ data/data.js
@@ -16,11 +16,35 @@ exports.events = [
 		canceled: ''
 	},
 	{
+		act: 'Lera Auerbach',
+		date: '2019-09-03',
+		type: 'Concert',
+		eventUrl: 'https://www.facebook.com/events/390648051635975/',
+		venueName: 'Filarmonica de Stat Transilvania',
+		canceled: ''
+	},
+	{
 		act: 'Morild',
 		date: '2019-09-03',
 		type: 'Concert',
 		eventUrl: 'https://www.facebook.com/events/467047377195122/',
 		venueName: 'Urania Palace',
+		canceled: ''
+	},
+	{
+		act: 'Charles Hamelin',
+		date: '2019-09-04',
+		type: 'Concert',
+		eventUrl: 'https://www.facebook.com/events/2611424115558459/',
+		venueName: 'Filarmonica de Stat Transilvania',
+		canceled: ''
+	},
+	{
+		act: 'Monte Piano Trio',
+		date: '2019-09-05',
+		type: 'Concert',
+		eventUrl: 'https://www.facebook.com/events/2201622280151580/',
+		venueName: 'Filarmonica de Stat Transilvania',
 		canceled: ''
 	},
 	{
@@ -40,35 +64,27 @@ exports.events = [
 		canceled: ''
 	},
 	{
+		act: 'Ziyu He',
+		date: '2019-09-08',
+		type: 'Concert',
+		eventUrl: 'https://www.facebook.com/events/583320192190470/',
+		venueName: 'Filarmonica de Stat Transilvania',
+		canceled: ''
+	},
+	{
+		act: 'Elisabeth Leonskaja',
+		date: '2019-09-10',
+		type: 'Concert',
+		eventUrl: 'https://www.facebook.com/events/457902801631810/',
+		venueName: 'Filarmonica de Stat Transilvania',
+		canceled: ''
+	},
+	{
 		act: 'Downfall of Gaia',
 		date: '2019-09-13',
 		type: 'Concert',
 		eventUrl: 'https://www.facebook.com/events/380775452415400/',
 		venueName: 'Form Space',
-		canceled: ''
-	},
-	{
-		act: 'Dans contemporan',
-		date: '2019-09-14',
-		type: 'Workshop',
-		eventUrl: 'https://www.facebook.com/events/477585419732259/',
-		venueName: 'Fabrica de Pensule',
-		canceled: ''
-	},
-	{
-		act: 'Shokran',
-		date: '2019-09-15',
-		type: 'Concert',
-		eventUrl: 'https://www.facebook.com/events/1369811869843692/',
-		venueName: 'Flying Circus',
-		canceled: ''
-	},
-	{
-		act: 'Kentawr',
-		date: '2019-09-16',
-		type: 'Concert',
-		eventUrl: 'https://www.facebook.com/events/879865329051520/',
-		venueName: 'Urania Palace',
 		canceled: ''
 	}
 ];
