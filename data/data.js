9ae8da93d9f352aae21d8483119118b7944f4e84
Drei <stoicescuofsuburbia@gmail.com>
2019-09-02 12:32:04 +0300

    Update events

--- data/data.js
+++ data/data.js
@@ -32,6 +32,14 @@ exports.events = [
 		canceled: ''
 	},
 	{
+		act: 'Apollo 11',
+		date: '2019-09-06',
+		type: 'Documentary',
+		eventUrl: 'https://www.facebook.com/events/483236582242121/',
+		venueName: 'Cinema Victoria',
+		canceled: ''
+	},
+	{
 		act: 'Dans contemporan',
 		date: '2019-09-07',
 		type: 'Workshop',
@@ -45,6 +53,14 @@ exports.events = [
 		type: 'DJ Set',
 		eventUrl: 'https://www.facebook.com/events/517000599069357/',
 		venueName: 'Gazette',
+		canceled: ''
+	},
+	{
+		act: 'Amorf',
+		date: '2019-09-07',
+		type: 'DJ Set',
+		eventUrl: 'https://www.facebook.com/events/755564701567720/',
+		venueName: 'Sala Polivalentă',
 		canceled: ''
 	},
 	{
@@ -69,6 +85,38 @@ exports.events = [
 		type: 'Concert',
 		eventUrl: 'https://www.facebook.com/events/380775452415400/',
 		venueName: 'Form Space',
+		canceled: ''
+	},
+	{
+		act: 'Dans contemporan',
+		date: '2019-09-14',
+		type: 'Workshop',
+		eventUrl: 'https://www.facebook.com/events/477585419732259/',
+		venueName: 'Fabrica de Pensule',
+		canceled: ''
+	},
+	{
+		act: 'Nobuyuki Tsujii',
+		date: '2019-09-14',
+		type: 'Concert',
+		eventUrl: 'https://www.facebook.com/events/619327428534323/',
+		venueName: 'Filarmonica de Stat Transilvania',
+		canceled: ''
+	},
+	{
+		act: 'Shokran',
+		date: '2019-09-15',
+		type: 'Concert',
+		eventUrl: 'https://www.facebook.com/events/1369811869843692/',
+		venueName: 'Flying Circus',
+		canceled: ''
+	},
+	{
+		act: 'Kentawr',
+		date: '2019-09-16',
+		type: 'Concert',
+		eventUrl: 'https://www.facebook.com/events/879865329051520/',
+		venueName: 'Urania Palace',
 		canceled: ''
 	}
 ];
