69de94248146401fd2dbf2954639741c58aaaf2e
Drei <stoicescuofsuburbia@gmail.com>
2019-09-06 13:54:13 +0300

    Update events

--- data/data.js
+++ data/data.js
@@ -1,13 +1,5 @@
 exports.events = [
 	{
-		act: 'Monte Piano Trio',
-		date: '2019-09-05',
-		type: 'Concert',
-		eventUrl: 'https://www.facebook.com/events/2201622280151580/',
-		venueName: 'Filarmonica de Stat Transilvania',
-		canceled: ''
-	},
-	{
 		act: 'Decizia',
 		date: '2019-09-06',
 		type: 'Performance Art',
