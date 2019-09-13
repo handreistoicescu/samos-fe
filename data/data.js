3b4d5236aa38acea2c665ece06200ea769d344cf
Drei <stoicescuofsuburbia@gmail.com>
2019-09-13 13:33:03 +0300

    Update events

--- data/data.js
+++ data/data.js
@@ -96,11 +96,27 @@ exports.events = [
 		canceled: ''
 	},
 	{
+		act: 'Fatima Hajji',
+		date: '2019-09-20',
+		type: 'DJ Set',
+		eventUrl: 'https://www.facebook.com/events/415697092396165/',
+		venueName: 'Club Midi',
+		canceled: ''
+	},
+	{
 		act: 'În umbra marelui plan',
 		date: '2019-09-21',
 		type: 'Theatre',
 		eventUrl: 'https://www.facebook.com/events/1677549462377441/',
 		venueName: 'REACTOR de creație și experiment',
+		canceled: ''
+	},
+	{
+		act: 'Jonny Rock',
+		date: '2019-09-21',
+		type: 'DJ Set',
+		eventUrl: 'https://www.facebook.com/events/431969964082740/',
+		venueName: 'Club Midi',
 		canceled: ''
 	},
 	{
@@ -125,38 +141,6 @@ exports.events = [
 		type: 'Concert',
 		eventUrl: 'https://www.facebook.com/events/380005205962990/',
 		venueName: 'Flying Circus',
-		canceled: ''
-	},
-	{
-		act: 'One Burning Match',
-		date: '2019-09-30',
-		type: 'Concert',
-		eventUrl: 'https://www.facebook.com/events/2557925154268082/',
-		venueName: 'Urania Palace',
-		canceled: ''
-	},
-	{
-		act: 'Sânziana și Pepelea',
-		date: '2019-10-01',
-		type: 'Theatre',
-		eventUrl: 'https://www.facebook.com/events/2204948262948074/',
-		venueName: 'Teatrul Naţional',
-		canceled: ''
-	},
-	{
-		act: 'Varsovie',
-		date: '2019-10-04',
-		type: 'Concert',
-		eventUrl: 'https://www.facebook.com/events/1659755900824780/',
-		venueName: 'Flying Circus',
-		canceled: ''
-	},
-	{
-		act: 'Cassus',
-		date: '2019-10-06',
-		type: 'Concert',
-		eventUrl: 'https://www.facebook.com/events/734478917021069/',
-		venueName: 'Urania Palace',
 		canceled: ''
 	}
 ];
