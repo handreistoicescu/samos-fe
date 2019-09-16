0a9a80267d3406baf3e69895052497c2d3d4b21b
Drei <stoicescuofsuburbia@gmail.com>
2019-09-16 18:40:12 +0300

    Update events

--- data/data.js
+++ data/data.js
@@ -1,53 +1,5 @@
 exports.events = [
 	{
-		act: 'Downfall of Gaia',
-		date: '2019-09-13',
-		type: 'Concert',
-		eventUrl: 'https://www.facebook.com/events/380775452415400/',
-		venueName: 'Form Space',
-		canceled: ''
-	},
-	{
-		act: 'Oana Cliţan',
-		date: '2019-09-13',
-		type: 'Vernissage',
-		eventUrl: 'https://www.facebook.com/events/2526962710867961/',
-		venueName: 'White Cuib',
-		canceled: ''
-	},
-	{
-		act: 'Dans contemporan',
-		date: '2019-09-14',
-		type: 'Workshop',
-		eventUrl: 'https://www.facebook.com/events/477585419732259/',
-		venueName: 'Fabrica de Pensule',
-		canceled: ''
-	},
-	{
-		act: 'Nobuyuki Tsujii',
-		date: '2019-09-14',
-		type: 'Concert',
-		eventUrl: 'https://www.facebook.com/events/619327428534323/',
-		venueName: 'Filarmonica de Stat Transilvania',
-		canceled: ''
-	},
-	{
-		act: 'Rumble Fish',
-		date: '2019-09-14',
-		type: 'Movie',
-		eventUrl: 'https://www.facebook.com/events/1386198151528427/',
-		venueName: 'Colectiva Gazette',
-		canceled: ''
-	},
-	{
-		act: 'Shokran',
-		date: '2019-09-15',
-		type: 'Concert',
-		eventUrl: 'https://www.facebook.com/events/1369811869843692/',
-		venueName: 'Flying Circus',
-		canceled: ''
-	},
-	{
 		act: 'Kentawr',
 		date: '2019-09-16',
 		type: 'Concert',
