7b6b423a27ef079ef4e7bf00be162f7e0b9fd834
Drei <stoicescuofsuburbia@gmail.com>
2019-09-25 09:12:44 +0300

    Update events

--- data/data.js
+++ data/data.js
@@ -38,5 +38,29 @@ exports.events = [
 		eventUrl: 'https://www.facebook.com/events/2204948262948074/',
 		venueName: 'Teatrul Naţional',
 		canceled: ''
+	},
+	{
+		act: 'Varsovie',
+		date: '2019-10-04',
+		type: 'Concert',
+		eventUrl: 'https://www.facebook.com/events/1659755900824780/',
+		venueName: 'Flying Circus',
+		canceled: ''
+	},
+	{
+		act: 'Cassus',
+		date: '2019-10-06',
+		type: 'Concert',
+		eventUrl: 'https://www.facebook.com/events/734478917021069/',
+		venueName: 'Urania Palace',
+		canceled: ''
+	},
+	{
+		act: 'Hooverphonic',
+		date: '2019-10-09',
+		type: 'Concert',
+		eventUrl: 'https://www.facebook.com/events/322095941840382/',
+		venueName: 'Form Space',
+		canceled: ''
 	}
 ];
