aad787450eabfae499d20ce042ffdebd82e1da29
Drei <stoicescuofsuburbia@gmail.com>
2019-10-04 16:03:00 +0300

    Update events

--- data/data.js
+++ data/data.js
@@ -1,13 +1,5 @@
 exports.events = [
 	{
-		act: 'Sânziana și Pepelea',
-		date: '2019-10-01',
-		type: 'Theatre',
-		eventUrl: 'https://www.facebook.com/events/2204948262948074/',
-		venueName: 'Teatrul Naţional',
-		canceled: ''
-	},
-	{
 		act: 'Varsovie',
 		date: '2019-10-04',
 		type: 'Concert',
