13f9113e2bddf3a6753cd8e76e92cfd274f840ff
Drei <stoicescuofsuburbia@gmail.com>
2019-10-04 16:37:20 +0300

    Update events

--- data/data.js
+++ data/data.js
@@ -1,5 +1,45 @@
 exports.events = [
 	{
+		act: 'SENSORIA',
+		date: '2019-10-04',
+		type: 'Vernissage',
+		eventUrl: 'https://www.facebook.com/events/2204981196280882/',
+		venueName: 'Urania Palace',
+		canceled: ''
+	},
+	{
+		act: 'Norbert Filep',
+		date: '2019-10-04',
+		type: 'Vernissage',
+		eventUrl: 'https://www.facebook.com/events/624697091390507/',
+		venueName: 'MATCA artspace',
+		canceled: ''
+	},
+	{
+		act: 'Blind Date #2',
+		date: '2019-10-04',
+		type: 'Vernissage',
+		eventUrl: 'https://www.facebook.com/events/2858173027530215/',
+		venueName: 'Lateral ArtSpace',
+		canceled: ''
+	},
+	{
+		act: 'Dries Segers',
+		date: '2019-10-04',
+		type: 'Vernissage',
+		eventUrl: 'https://www.facebook.com/events/529345184467638/',
+		venueName: 'Camera',
+		canceled: ''
+	},
+	{
+		act: 'Ivo Dimchev',
+		date: '2019-10-04',
+		type: 'Performance Art',
+		eventUrl: 'https://www.facebook.com/events/667840573713061/',
+		venueName: 'Fabrica de Pensule',
+		canceled: ''
+	},
+	{
 		act: 'Varsovie',
 		date: '2019-10-04',
 		type: 'Concert',
@@ -8,7 +48,47 @@ exports.events = [
 		canceled: ''
 	},
 	{
-		act: 'Cassus',
+		act: 'Herodot',
+		date: '2019-10-04',
+		type: 'DJ Set',
+		eventUrl: 'https://www.facebook.com/events/247999449463356/',
+		venueName: 'Mioritmic',
+		canceled: ''
+	},
+	{
+		act: 'Totul e bine când se termină',
+		date: '2019-10-05',
+		type: 'Theatre',
+		eventUrl: 'https://www.facebook.com/events/2597440460294853/',
+		venueName: 'REACTOR de creație și experiment',
+		canceled: ''
+	},
+	{
+		act: 'von Bülove',
+		date: '2019-10-05',
+		type: 'DJ Set',
+		eventUrl: 'https://www.facebook.com/events/2423546537916478/',
+		venueName: 'Colectiva Gazette',
+		canceled: ''
+	},
+	{
+		act: 'Sit',
+		date: '2019-10-05',
+		type: 'DJ Set',
+		eventUrl: 'https://www.facebook.com/events/247999449463356/',
+		venueName: 'Mioritmic',
+		canceled: ''
+	},
+	{
+		act: 'ABUZ - mâine plec acasă',
+		date: '2019-10-06',
+		type: 'Theatre',
+		eventUrl: 'https://www.facebook.com/events/240624160197237/',
+		venueName: 'REACTOR de creație și experiment',
+		canceled: ''
+	},
+	{
+		act: 'Cold Brats',
 		date: '2019-10-06',
 		type: 'Concert',
 		eventUrl: 'https://www.facebook.com/events/734478917021069/',
@@ -16,10 +96,146 @@ exports.events = [
 		canceled: ''
 	},
 	{
+		act: 'Arapu',
+		date: '2019-10-06',
+		type: 'DJ Set',
+		eventUrl: 'https://www.facebook.com/events/247999449463356/',
+		venueName: 'Mioritmic',
+		canceled: ''
+	},
+	{
+		act: 'JOKER',
+		date: '2019-10-07',
+		type: 'Movie',
+		eventUrl: 'https://www.facebook.com/events/1034672666703152/',
+		venueName: 'Cinema Victoria',
+		canceled: ''
+	},
+	{
+		act: 'Calatoria Fantastică a Maronei',
+		date: '2019-10-08',
+		type: 'Movie',
+		eventUrl: 'https://www.facebook.com/events/722951794817641/',
+		venueName: 'Cinema Victoria',
+		canceled: ''
+	},
+	{
+		act: 'Ovidiu Anton',
+		date: '2019-10-09',
+		type: 'Discussion',
+		eventUrl: 'https://www.facebook.com/events/519574281943899/',
+		venueName: 'Fabrica de Pensule',
+		canceled: ''
+	},
+	{
 		act: 'Hooverphonic',
 		date: '2019-10-09',
 		type: 'Concert',
 		eventUrl: 'https://www.facebook.com/events/322095941840382/',
+		venueName: 'Form Space',
+		canceled: ''
+	},
+	{
+		act: 'Mirage',
+		date: '2019-10-10',
+		type: 'Performance Art',
+		eventUrl: 'https://www.facebook.com/events/430832624219905/',
+		venueName: 'Universitatea Sapienția',
+		canceled: ''
+	},
+	{
+		act: '99,6%',
+		date: '2019-10-10',
+		type: 'Theatre',
+		eventUrl: 'https://www.facebook.com/events/1391164044384793/',
+		venueName: 'REACTOR de creație și experiment',
+		canceled: ''
+	},
+	{
+		act: 'Renegades Of Jazz',
+		date: '2019-10-10',
+		type: 'Concert',
+		eventUrl: 'https://www.facebook.com/events/2122963434665218/',
+		venueName: 'Atelier Cafe',
+		canceled: ''
+	},
+	{
+		act: 'Enric Fort Ballester',
+		date: '2019-10-11',
+		type: 'Vernissage',
+		eventUrl: 'https://www.facebook.com/events/2374257079492147/',
+		venueName: 'Quadro 21 Gallery',
+		canceled: ''
+	},
+	{
+		act: 'BLIND DATE #3',
+		date: '2019-10-11',
+		type: 'Vernissage',
+		eventUrl: 'https://www.facebook.com/events/2829763377053395/',
+		venueName: 'Lateral ArtSpace',
+		canceled: ''
+	},
+	{
+		act: 'Totally F***ed Up',
+		date: '2019-10-11',
+		type: 'Movie',
+		eventUrl: 'https://www.facebook.com/events/1466593046813693/',
+		venueName: 'Cinema Arta',
+		canceled: ''
+	},
+	{
+		act: 'Body of Defiance',
+		date: '2019-10-12',
+		type: 'Theatre',
+		eventUrl: 'https://www.facebook.com/events/2391777727705584/',
+		venueName: 'REACTOR de creație și experiment',
+		canceled: ''
+	},
+	{
+		act: 'Tarnation',
+		date: '2019-10-12',
+		type: 'Movie',
+		eventUrl: 'https://www.facebook.com/events/1466593046813693/',
+		venueName: 'Cinema Arta',
+		canceled: ''
+	},
+	{
+		act: 'Micul nostru centenar',
+		date: '2019-10-13',
+		type: 'Theatre',
+		eventUrl: 'https://www.facebook.com/events/606351703102284/',
+		venueName: 'REACTOR de creație și experiment',
+		canceled: ''
+	},
+	{
+		act: 'The Watermelon Woman',
+		date: '2019-10-13',
+		type: 'Movie',
+		eventUrl: 'https://www.facebook.com/events/1466593046813693/',
+		venueName: 'Cinema Arta',
+		canceled: ''
+	},
+	{
+		act: 'Thomas Bîrzan',
+		date: '2019-10-14',
+		type: 'Workshop',
+		eventUrl: 'https://www.facebook.com/events/386270928929859/',
+		venueName: 'Fabrica de Pensule',
+		canceled: ''
+	},
+	{
+		act: 'Thomas Bîrzan',
+		date: '2019-10-15',
+		type: 'Workshop',
+		eventUrl: 'https://www.facebook.com/events/386270928929859/',
+		venueName: 'Fabrica de Pensule',
+		canceled: ''
+	},
+	{
+		act: 'Föllakzoid',
+		date: '2019-10-15',
+		type: 'Live Set',
+		eventUrl: 'https://www.facebook.com/events/950164308697397/',
 		venueName: 'Form Space',
 		canceled: ''
 	}
