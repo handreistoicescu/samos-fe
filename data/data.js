0725e69566059ddf1fb1475fba21cc71ab4ab682
Drei <stoicescuofsuburbia@gmail.com>
2019-11-01 13:21:56 +0200

    Update events

--- data/data.js
+++ data/data.js
@@ -1,170 +1,122 @@
 exports.events = [
 	{
-		act: "M. Houellebecq",
-		date: "2019-10-30",
-		type: "Book club",
-		eventUrl: "https://www.facebook.com/events/402044987145346/",
-		venueName: "Colectiva Gazette",
-		canceled: ""
+		act: 'Sheldon Drake',
+		date: '2019-11-01',
+		type: 'Live Set',
+		eventUrl: 'https://www.facebook.com/events/384025882483859/',
+		venueName: 'Gazette',
+		canceled: ''
 	},
 	{
-		act: "Sărățele și friptură: o serată cu Decebal și Traian",
-		date: "2019-10-30",
-		type: "Reader's theatre",
-		eventUrl: "https://www.facebook.com/events/2551601025063441/",
-		venueName: "Carturești Iulius",
-		canceled: ""
+		act: 'Saboar',
+		date: '2019-11-01',
+		type: 'DJ Set',
+		eventUrl: 'https://www.facebook.com/events/522826144929354/',
+		venueName: 'Urania Palace',
+		canceled: ''
 	},
 	{
-		act: "Limite",
-		date: "2019-10-30",
-		type: "Performance Art",
-		eventUrl: "https://www.facebook.com/events/2530561313657632/",
-		venueName: "Universitatea Sapienția",
-		canceled: ""
+		act: 'Totul e bine când se termină',
+		date: '2019-11-02',
+		type: 'Theatre',
+		eventUrl: 'https://www.facebook.com/events/1402631273224731/',
+		venueName: 'REACTOR de creație și experiment',
+		canceled: ''
 	},
 	{
-		act: "Final Cut: Ladies and Gentlemen",
-		date: "2019-10-30",
-		type: "Movie",
-		eventUrl: "https://www.facebook.com/events/781923665598117",
-		venueName: "Cinema Arta",
-		canceled: ""
+		act: 'Aeminium',
+		date: '2019-11-02',
+		type: 'DJ Set',
+		eventUrl: 'https://www.facebook.com/events/434091853907177/',
+		venueName: 'Urania Palace',
+		canceled: ''
 	},
 	{
-		act: "Toată liniștea din lume",
-		date: "2019-10-31",
-		type: "Theatre",
-		eventUrl: "https://www.facebook.com/events/470575410212407/",
-		venueName: "Casa Tranzit",
-		canceled: ""
+		act: 'PE EI!',
+		date: '2019-11-03',
+		type: 'Theatre',
+		eventUrl: 'https://www.facebook.com/events/2157734934528821/',
+		venueName: 'Casa Tranzit',
+		canceled: ''
 	},
 	{
-		act: "Very Cool People",
-		date: "2019-10-31",
-		type: "Concert",
-		eventUrl: "https://www.facebook.com/events/1304469489735282/",
-		venueName: "Atelier Cafe",
-		canceled: ""
+		act: 'NEVERLAND',
+		date: '2019-11-05',
+		type: 'Performance Art',
+		eventUrl: 'https://www.facebook.com/events/2404863549611911/',
+		venueName: 'ZUG.zone',
+		canceled: ''
 	},
 	{
-		act: "Poethree",
-		date: "2019-10-31",
-		type: "Open mic",
-		eventUrl: "https://www.facebook.com/events/1828753970601705/",
-		venueName: "REACTOR de creație și experiment",
-		canceled: ""
+		act: 'Shahmen',
+		date: '2019-11-06',
+		type: 'Concert',
+		eventUrl: 'https://www.facebook.com/events/416138199018719/',
+		venueName: 'Form Space',
+		canceled: ''
 	},
 	{
-		act: "DJ Crumbs",
-		date: "2019-10-31",
-		type: "DJ Set",
-		eventUrl: "https://www.facebook.com/events/730026980846452/",
-		venueName: "Gazette",
-		canceled: ""
+		act: 'Nightstalker',
+		date: '2019-11-07',
+		type: 'Concert',
+		eventUrl: 'https://www.facebook.com/events/2175394965919529/',
+		venueName: 'Flying Circus',
+		canceled: ''
 	},
 	{
-		act: "Sheldon Drake",
-		date: "2019-11-01",
-		type: "Live Set",
-		eventUrl: "https://www.facebook.com/events/384025882483859/",
-		venueName: "Gazette",
-		canceled: ""
+		act: 'Nunki Bay Starship',
+		date: '2019-11-07',
+		type: 'Concert',
+		eventUrl: 'https://www.facebook.com/events/541626589935639/',
+		venueName: 'Atelier Cafe',
+		canceled: ''
 	},
 	{
-		act: "Saboar",
-		date: "2019-11-01",
-		type: "DJ Set",
-		eventUrl: "https://www.facebook.com/events/522826144929354/",
-		venueName: "Urania Palace",
-		canceled: ""
+		act: 'E-an-na',
+		date: '2019-11-08',
+		type: 'Concert',
+		eventUrl: 'https://www.facebook.com/events/641538432993161/',
+		venueName: 'Flying Circus',
+		canceled: ''
 	},
 	{
-		act: "Totul e bine când se termină",
-		date: "2019-11-02",
-		type: "Theatre",
-		eventUrl: "https://www.facebook.com/events/1402631273224731/",
-		venueName: "REACTOR de creație și experiment",
-		canceled: ""
+		act: 'Amenra',
+		date: '2019-11-09',
+		type: 'Concert',
+		eventUrl: 'https://www.facebook.com/events/472093046870679/',
+		venueName: 'Form Space',
+		canceled: ''
 	},
 	{
-		act: "Aeminium",
-		date: "2019-11-02",
-		type: "DJ Set",
-		eventUrl: "https://www.facebook.com/events/434091853907177/",
-		venueName: "Urania Palace",
-		canceled: ""
+		act: 'Dulce-acrișor',
+		date: '2019-11-12',
+		type: 'Theatre',
+		eventUrl: 'https://www.facebook.com/events/257345365194932/',
+		venueName: 'ZUG.zone',
+		canceled: ''
 	},
 	{
-		act: "PE EI!",
-		date: "2019-11-03",
-		type: "Theatre",
-		eventUrl: "https://www.facebook.com/events/2157734934528821/",
-		venueName: "Casa Tranzit",
-		canceled: ""
+		act: 'Bogdan Vaida',
+		date: '2019-11-12',
+		type: 'Concert',
+		eventUrl: 'https://www.facebook.com/events/1505409752935603/',
+		venueName: 'REACTOR de creație și experiment',
+		canceled: ''
 	},
 	{
-		act: "NEVERLAND",
-		date: "2019-11-05",
-		type: "Performance Art",
-		eventUrl: "https://www.facebook.com/events/2404863549611911/",
-		venueName: "ZUG.zone",
-		canceled: ""
+		act: 'R.O x Konoba',
+		date: '2019-11-14',
+		type: 'Concert',
+		eventUrl: 'https://www.facebook.com/events/696202424209882/',
+		venueName: 'Form Space',
+		canceled: ''
 	},
 	{
-		act: "Shahmen",
-		date: "2019-11-06",
-		type: "Concert",
-		eventUrl: "https://www.facebook.com/events/416138199018719/",
-		venueName: "Form Space",
-		canceled: ""
-	},
-	{
-		act: "Nightstalker",
-		date: "2019-11-07",
-		type: "Concert",
-		eventUrl: "https://www.facebook.com/events/2175394965919529/",
-		venueName: "Flying Circus",
-		canceled: ""
-	},
-	{
-		act: "Nunki Bay Starship",
-		date: "2019-11-07",
-		type: "Concert",
-		eventUrl: "https://www.facebook.com/events/541626589935639/",
-		venueName: "Atelier Cafe",
-		canceled: ""
-	},
-	{
-		act: "E-an-na",
-		date: "2019-11-08",
-		type: "Concert",
-		eventUrl: "https://www.facebook.com/events/641538432993161/",
-		venueName: "Flying Circus",
-		canceled: ""
-	},
-	{
-		act: "Amenra",
-		date: "2019-11-09",
-		type: "Concert",
-		eventUrl: "https://www.facebook.com/events/472093046870679/",
-		venueName: "Form Space",
-		canceled: ""
-	},
-	{
-		act: "Dulce-acrișor",
-		date: "2019-11-12",
-		type: "Theatre",
-		eventUrl: "https://www.facebook.com/events/257345365194932/",
-		venueName: "ZUG.zone",
-		canceled: ""
-	},
-	{
-		act: "Bogdan Vaida",
-		date: "2019-11-12",
-		type: "Concert",
-		eventUrl: "https://www.facebook.com/events/1505409752935603/",
-		venueName: "REACTOR de creație și experiment",
-		canceled: ""
+		act: 'Max Abysmal',
+		date: '2019-11-16',
+		type: 'DJ Set',
+		eventUrl: 'https://www.facebook.com/events/458883744978365/',
+		venueName: 'Gazette',
+		canceled: ''
 	}
 ];
