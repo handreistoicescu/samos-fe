6f20d373a06348a1783c4ee7bf9dc00a8fa265b9
Drei <stoicescuofsuburbia@gmail.com>
2019-11-16 08:02:56 +0200

    Update events

--- data/data.js
+++ data/data.js
@@ -1,37 +1,5 @@
 exports.events = [
 	{
-		act: 'Amenra',
-		date: '2019-11-09',
-		type: 'Concert',
-		eventUrl: 'https://www.facebook.com/events/472093046870679/',
-		venueName: 'Form Space',
-		canceled: ''
-	},
-	{
-		act: 'Dulce-acrișor',
-		date: '2019-11-12',
-		type: 'Theatre',
-		eventUrl: 'https://www.facebook.com/events/257345365194932/',
-		venueName: 'ZUG.zone',
-		canceled: ''
-	},
-	{
-		act: 'Bogdan Vaida',
-		date: '2019-11-12',
-		type: 'Concert',
-		eventUrl: 'https://www.facebook.com/events/1505409752935603/',
-		venueName: 'REACTOR de creație și experiment',
-		canceled: ''
-	},
-	{
-		act: 'R.O x Konoba',
-		date: '2019-11-14',
-		type: 'Concert',
-		eventUrl: 'https://www.facebook.com/events/696202424209882/',
-		venueName: 'Form Space',
-		canceled: ''
-	},
-	{
 		act: 'Max Abysmal',
 		date: '2019-11-16',
 		type: 'DJ Set',
