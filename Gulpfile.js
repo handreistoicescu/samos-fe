ffef30047029ad4c7cba68076cada9f19b0cc220
Andrei Stoicescu <stoicescuofsuburbia@gmail.com>
2021-07-20 03:28:01 +0300

    Update dependencies and everything

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,32 +1,32 @@
-'use strict';
+"use strict";
 
 // -----------------------------------------------------------------------------
 // Dependencies
 // -----------------------------------------------------------------------------
 
-var gulp = require('gulp');
-var data = require('gulp-data');
-var sass = require('gulp-sass');
-var realFavicon = require('gulp-real-favicon');
-var fs = require('fs');
-var browserSync = require('browser-sync').create();
-var nunjucksRender = require('gulp-nunjucks-render');
-var dateFilter = require('nunjucks-date-filter');
-var concat = require('gulp-concat');
-var siteOutput = './dist';
+var gulp = require("gulp");
+var data = require("gulp-data");
+var sass = require("gulp-sass")(require("sass"));
+var realFavicon = require("gulp-real-favicon");
+var fs = require("fs");
+var browserSync = require("browser-sync").create();
+var nunjucksRender = require("gulp-nunjucks-render");
+var dateFilter = require("nunjucks-date-filter");
+var concat = require("gulp-concat");
+var siteOutput = "./dist";
 
 // -----------------------------------------------------------------------------
 // Configuration
 // -----------------------------------------------------------------------------
 
-var inputMain = './scss/main.scss';
-var output = siteOutput + '/css';
-var inputTemplates = './pages/*.html';
-var sassOptions = { outputStyle: 'expanded' };
-var samosEvents = require('./data/data');
+var inputMain = "./scss/main.scss";
+var output = siteOutput + "/css";
+var inputTemplates = "./pages/*.html";
+var sassOptions = { outputStyle: "expanded" };
+var samosEvents = require("./data/data");
 
 // File where the favicon markups are stored
-var FAVICON_DATA_FILE = 'faviconData.json';
+var FAVICON_DATA_FILE = "faviconData.json";
 
 // -----------------------------------------------------------------------------
 // Sass compilation
@@ -35,7 +35,7 @@ var FAVICON_DATA_FILE = 'faviconData.json';
 function sassTask() {
 	return gulp
 		.src(inputMain)
-		.pipe(sass(sassOptions).on('error', sass.logError))
+		.pipe(sass(sassOptions).on("error", sass.logError))
 		.pipe(gulp.dest(output))
 		.pipe(browserSync.stream());
 }
@@ -46,10 +46,10 @@ function sassTask() {
 
 function scriptsTask() {
 	return gulp
-		.src(['js/main.js'])
-		.pipe(concat({ path: 'main.js' }))
+		.src(["js/main.js"])
+		.pipe(concat({ path: "main.js" }))
 		.pipe(browserSync.reload({ stream: true }))
-		.pipe(gulp.dest(siteOutput + '/js'));
+		.pipe(gulp.dest(siteOutput + "/js"));
 }
 
 // -----------------------------------------------------------------------------
@@ -57,13 +57,13 @@ function scriptsTask() {
 // -----------------------------------------------------------------------------
 
 function nunjucksTask() {
-	var manageEnvironment = function(environment) {
-		environment.addFilter('date', dateFilter);
+	var manageEnvironment = function (environment) {
+		environment.addFilter("date", dateFilter);
 	};
 
-	const getDataForFile = function(file) {
+	const getDataForFile = function (file) {
 		return {
-			events: samosEvents.events
+			events: samosEvents.events,
 		};
 	};
 
@@ -75,8 +75,8 @@ function nunjucksTask() {
 			// Renders template with nunjucks
 			.pipe(
 				nunjucksRender({
-					path: ['./templates/'],
-					manageEnv: manageEnvironment
+					path: ["./templates/"],
+					manageEnv: manageEnvironment,
 				})
 			)
 			// add favicons data
@@ -102,67 +102,67 @@ function generateFavIconTask(done) {
 	{
 		realFavicon.generateFavicon(
 			{
-				masterPicture: 'assets/favicons/source/master_picture.png',
-				dest: 'assets/favicons/dist',
-				iconsPath: '/',
+				masterPicture: "assets/favicons/source/master_picture.png",
+				dest: "assets/favicons/dist",
+				iconsPath: "/",
 				design: {
 					ios: {
-						pictureAspect: 'backgroundAndMargin',
-						backgroundColor: '#004853',
-						margin: '14%',
+						pictureAspect: "backgroundAndMargin",
+						backgroundColor: "#004853",
+						margin: "14%",
 						assets: {
 							ios6AndPriorIcons: false,
 							ios7AndLaterIcons: false,
 							precomposedIcons: false,
-							declareOnlyDefaultIcon: true
-						}
+							declareOnlyDefaultIcon: true,
+						},
 					},
 					desktopBrowser: {},
 					windows: {
-						pictureAspect: 'noChange',
-						backgroundColor: '#2b5797',
-						onConflict: 'override',
+						pictureAspect: "noChange",
+						backgroundColor: "#2b5797",
+						onConflict: "override",
 						assets: {
 							windows80Ie10Tile: false,
 							windows10Ie11EdgeTiles: {
 								small: false,
 								medium: true,
 								big: false,
-								rectangle: false
-							}
-						}
+								rectangle: false,
+							},
+						},
 					},
 					androidChrome: {
-						pictureAspect: 'backgroundAndMargin',
-						margin: '17%',
-						backgroundColor: '#004853',
-						themeColor: '#004853',
+						pictureAspect: "backgroundAndMargin",
+						margin: "17%",
+						backgroundColor: "#004853",
+						themeColor: "#004853",
 						manifest: {
-							display: 'standalone',
-							orientation: 'notSet',
-							onConflict: 'override',
-							declared: true
+							display: "standalone",
+							orientation: "notSet",
+							onConflict: "override",
+							declared: true,
 						},
 						assets: {
 							legacyIcon: false,
-							lowResolutionIcons: false
-						}
+							lowResolutionIcons: false,
+						},
 					},
 					safariPinnedTab: {
-						pictureAspect: 'silhouette',
-						themeColor: '#004853'
-					}
+						pictureAspect: "silhouette",
+						themeColor: "#004853",
+					},
 				},
 				settings: {
-					scalingAlgorithm: 'Mitchell',
+					scalingAlgorithm: "Mitchell",
 					errorOnImageTooSmall: false,
 					readmeFile: false,
 					htmlCodeFile: false,
-					usePathAsIs: false
+					usePathAsIs: false,
 				},
-				markupFile: FAVICON_DATA_FILE
+				markupFile: FAVICON_DATA_FILE,
 			},
-			function() {
+			function () {
 				done();
 			}
 		);
@@ -176,7 +176,7 @@ function generateFavIconTask(done) {
 
 function checkForFaviconUpdateTask(done) {
 	var currentVersion = JSON.parse(fs.readFileSync(FAVICON_DATA_FILE)).version;
-	realFavicon.checkForUpdates(currentVersion, function(err) {
+	realFavicon.checkForUpdates(currentVersion, function (err) {
 		if (err) {
 			throw err;
 		}
@@ -185,7 +185,7 @@ function checkForFaviconUpdateTask(done) {
 
 // Copy favicons to dist folder
 function copyFaviconsTask() {
-	return gulp.src('./assets/favicons/dist/*').pipe(gulp.dest(siteOutput));
+	return gulp.src("./assets/favicons/dist/*").pipe(gulp.dest(siteOutput));
 }
 
 // -----------------------------------------------------------------------------
@@ -195,12 +195,12 @@ function copyFaviconsTask() {
 function watchTask() {
 	// Watch the sass input folder for change,
 	// and run `sass` task when something happens
-	gulp.watch('./scss/*.scss', sassTask);
+	gulp.watch("./scss/*.scss", sassTask);
 
-	gulp.watch('./js/*', scriptsTask).on('change', browserSync.reload);
+	gulp.watch("./js/*", scriptsTask).on("change", browserSync.reload);
 
 	// Watch nunjuck templates and reload browser if change
-	gulp.watch(inputTemplates, nunjucksTask).on('change', browserSync.reload);
+	gulp.watch(inputTemplates, nunjucksTask).on("change", browserSync.reload);
 }
 
 // -----------------------------------------------------------------------------
@@ -210,8 +210,8 @@ function watchTask() {
 function browserSyncTask() {
 	browserSync.init({
 		server: {
-			baseDir: siteOutput
-		}
+			baseDir: siteOutput,
+		},
 	});
 }
 
@@ -224,8 +224,8 @@ exports.default = gulp.series(
 	nunjucksTask,
 	scriptsTask,
 	copyFaviconsTask,
-	watchTask,
-	browserSyncTask
+	browserSyncTask,
+	watchTask
 );
 
 exports.build = gulp.series(
