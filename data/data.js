264415a571c7542f07a0088b6c0668535307803f
Andrei Stoicescu <stoicescuofsuburbia@gmail.com>
2021-08-03 11:20:56 +0300

    Update events

--- data/data.js
+++ data/data.js
@@ -4,14 +4,6 @@ exports.header_tagline_uptodate = "August 2021";
 
 exports.events = [
 	{
-		act: "Daniela Groza",
-		date: "2021-08-02",
-		type: "Photography Workshop",
-		eventUrl: "https://www.facebook.com/events/1170102563486541",
-		venueName: "Casa Tranzit",
-		canceled: "",
-	},
-	{
 		act: "Bastos",
 		date: "2021-08-07",
 		type: "Concert",
