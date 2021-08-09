00aacb1b3c4275cc19f76de8285e8a6c38e12c5d
Andrei Stoicescu <stoicescuofsuburbia@gmail.com>
2021-08-09 12:15:54 +0300

    Update events

--- data/data.js
+++ data/data.js
@@ -4,46 +4,6 @@ exports.header_tagline_uptodate = "August 2021";
 
 exports.events = [
 	{
-		act: "Artrax @ Electric Castle",
-		date: "2021-08-08",
-		type: "DJ Set",
-		eventUrl: "https://electriccastle.com/ec-special-schedule",
-		venueName: "Bonțida",
-		canceled: "",
-	},
-	{
-		act: "Mistajam @ Electric Castle",
-		date: "2021-08-08",
-		type: "DJ Set",
-		eventUrl: "https://electriccastle.com/ec-special-schedule",
-		venueName: "Bonțida",
-		canceled: "",
-	},
-	{
-		act: "Matador @ Electric Castle",
-		date: "2021-08-08",
-		type: "DJ Set",
-		eventUrl: "https://electriccastle.com/ec-special-schedule",
-		venueName: "Bonțida",
-		canceled: "",
-	},
-	{
-		act: "Keiyaa @ Electric Castle",
-		date: "2021-08-08",
-		type: "DJ Set",
-		eventUrl: "https://electriccastle.com/ec-special-schedule",
-		venueName: "Bonțida",
-		canceled: "",
-	},
-	{
-		act: "Fazer @ Electric Castle",
-		date: "2021-08-08",
-		type: "DJ Set",
-		eventUrl: "https://electriccastle.com/ec-special-schedule",
-		venueName: "Bonțida",
-		canceled: "",
-	},
-	{
 		act: "Zimbru @ Electric Castle",
 		date: "2021-08-09",
 		type: "Concert",
